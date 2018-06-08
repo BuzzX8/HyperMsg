f75240047e00e7d433f08a499661c1b145f83b70
Maxim Chasnik <maxim.chasnik@gmail.com>
2018-06-08 15:47:21 +0300

    Added CompositeMessageHandler

---
+++ src/HyperMsg.Core/CompositeMessageHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HyperMsg
+{
+    public class CompositeMessageHandler : IMessageHandlerRegistry
+    {
+		private readonly List<object> handlers = new List<object>();
+		private readonly List<object> asyncHandlers = new List<object>();
+	    
+	    public void Handle<T>(T message)
+	    {
+		    foreach (var handler in handlers.OfType<Action<T>>())
+		    {
+			    handler(message);
+		    }
+
+		    var tasks = asyncHandlers
+			    .OfType<Func<T, Task>>()
+			    .Select(h => h(message))
+			    .ToArray();
+
+		    Task.WaitAll(tasks);
+	    }
+		
+	    public void Register<T>(Action<T> handler)
+	    {
+		    handlers.Add(handler);
+	    }
+
+	    public void Register<T>(Action<T> handler, Func<T, bool> filter)
+	    {
+		    Register((Action<T>)new ConditionalMessageHandler<T>(handler, filter).Handle);
+	    }
+
+	    public void Register<T>(Func<T, Task> handler)
+	    {
+		    asyncHandlers.Add(handler);
+	    }
+
+	    public void Register<T>(Func<T, Task> handler, Func<T, bool> filter)
+	    {
+		    Register((Func<T, Task>)new ConditionalMessageHandler<T>(handler, filter).HandleAsync);
+	    }
+    }
+}
