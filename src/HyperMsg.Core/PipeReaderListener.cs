0a1b0ea58dfee8b5e3933c4d8f3262959714f10b
Maxim Chasnik <Maxim.Chasnik@wolterskluwer.com>
2018-10-10 14:09:40 +0300

    Added more tests for listener

--- src/HyperMsg.Core/PipeReaderListener.cs
+++ src/HyperMsg.Core/PipeReaderListener.cs
@@ -9,26 +9,35 @@ namespace HyperMsg
     public class PipeReaderListener
     {
         private readonly PipeReader pipeReader;
-        private readonly Func<ReadOnlySequence<byte>, int> byteReader;
+        private readonly Func<ReadOnlySequence<byte>, int> bufferReader;
 
         private CancellationTokenSource tokenSource;
         private Task listeningTask;
 
-        public PipeReaderListener(PipeReader pipeReader, Func<ReadOnlySequence<byte>, int> byteReader)
+        public PipeReaderListener(PipeReader pipeReader, Func<ReadOnlySequence<byte>, int> bufferReader)
         {
             this.pipeReader = pipeReader ?? throw new ArgumentNullException(nameof(pipeReader));
-            this.byteReader = byteReader ?? throw new ArgumentNullException(nameof(byteReader));
+            this.bufferReader = bufferReader ?? throw new ArgumentNullException(nameof(bufferReader));
             tokenSource = new CancellationTokenSource();
         }
 
+		public bool IsListening => listeningTask != null;
+
         public void Start()
         {
-            listeningTask = Task.Run(ListenReader);
+            listeningTask = Task.Run(ListenReader).ContinueWith(t =>
+            {
+	            OnError(t.Exception);
+				Stop();
+            }, TaskContinuationOptions.OnlyOnFaulted);
+			OnStarted();
         }
 
         public void Stop()
         {
-
+			tokenSource.Cancel();
+			listeningTask = null;
+			OnStopped();
         }
         
         private async Task ListenReader()
@@ -38,8 +47,50 @@ namespace HyperMsg
             while(!token.IsCancellationRequested)
             {
                 var result = await pipeReader.ReadAsync(token);
-                byteReader(result.Buffer);
-            }
+
+				if (result.IsCompleted)
+				{
+					listeningTask = null;
+					OnStopped();
+					return;
+				}
+
+				var bytesReaded = bufferReader(result.Buffer);
+				OnBufferReaded();
+
+				if (bytesReaded == 0)
+				{
+					continue;
+				}
+
+	            var position = result.Buffer.GetPosition(bytesReaded);
+	            pipeReader.AdvanceTo(position);
+			}
         }
+
+		private void OnStarted()
+		{
+			Started?.Invoke(this, EventArgs.Empty);
+		}
+
+		private void OnStopped()
+		{
+			Stopped?.Invoke(this, EventArgs.Empty);
+		}
+
+		private void OnBufferReaded()
+		{
+			BufferReaded?.Invoke(this, EventArgs.Empty);
+		}
+
+	    private void OnError(AggregateException exception)
+	    {
+			Error?.Invoke(this, EventArgs.Empty);
+	    }
+
+		public event EventHandler Started;
+		public event EventHandler Stopped;
+		public event EventHandler BufferReaded;
+	    public event EventHandler Error;
     }
 }
