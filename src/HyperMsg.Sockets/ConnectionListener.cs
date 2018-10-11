dbd40f4d0a9d4cc21ea1b76ce133d9196a9c9824
Maxim Chasnik <maxim.chasnik@outlook.com>
2018-10-11 22:42:06 +0300

    Initial commit

---
+++ src/HyperMsg.Sockets/ConnectionListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg.Sockets
+{
+    public class ConnectionListener : ObservableListener<SocketProxy>
+    {
+        private Lazy<Socket> socket;
+
+        public ConnectionListener(Func<Socket> socketFactory, IObserver<SocketProxy> observer) : base(observer)
+        {
+            socket = new Lazy<Socket>(socketFactory);
+        }
+
+        protected Socket Socket => socket.Value;
+
+        protected override async Task DoListening(CancellationToken token)
+        {
+            while(!token.IsCancellationRequested)
+            {
+                var result = await Task.Run(() => Socket.Accept());
+                OnNext(new SocketProxy(result));
+            }
+        }
+    }
+}
---
+++ src/HyperMsg.Sockets/SocketProxy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace HyperMsg.Sockets
+{
+    public class SocketProxy : IDisposable
+    {
+        private readonly Lazy<Socket> socket;        
+        private readonly EndPoint endpoint;
+
+        public SocketProxy(Func<Socket> socketFactory, EndPoint endpoint)
+        {
+            socket = new Lazy<Socket>(socketFactory);
+            this.endpoint = endpoint;
+        }
+
+        internal SocketProxy(Socket socket)
+        {
+            this.socket = new Lazy<Socket>(() => socket);
+        }
+
+        protected Socket Socket => socket.Value;
+
+        public Stream Stream { get; private set; }
+
+        public void Connect()
+        {
+            Socket.Connect(endpoint);
+            Stream = new NetworkStream(Socket);
+        }
+
+        public void Disconnect() => Socket.Disconnect(false);
+
+        public void Dispose() => Disconnect();
+    }
+}
