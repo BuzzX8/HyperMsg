41458d4f0c0de0ddd865f71ad8852411bd643752
Maxim Chasnik <maxim.chasnik@gmail.com>
2018-10-13 22:11:40 +0300

    Added MessageBuffer (#9)

---
+++ src/HyperMsg.Core/IMessageBuffer.cs
@@ -0,0 +1,11 @@
+﻿using System.IO.Pipelines;
+using System.Threading.Tasks;
+
+namespace HyperMsg
+{
+    interface IMessageBuffer<T>
+    {
+        void Write(T message);
+        Task<FlushResult> FlushAsync();
+    }
+}
