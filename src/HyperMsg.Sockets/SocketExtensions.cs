5a61f46aafa758de1bb93539d38f2d5c676f6d6b
Maxim Chasnik <maxim.chasnik@outlook.com>
2018-10-25 15:15:58 +0300

    Added SocketExtensions

--- src/HyperMsg.Sockets/SocketExtensions.cs
+++ src/HyperMsg.Sockets/SocketExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HyperMsg.Sockets
@@ -7,22 +9,42 @@ namespace HyperMsg.Sockets
     {
         public static int Read(this ISocket socket, Memory<byte> buffer)
         {
-            throw new Exception();
+            if (MemoryMarshal.TryGetArray<byte>(buffer, out var segment))
+            {
+                return socket.Stream.Read(segment.Array, segment.Offset, segment.Count);
+            }
+
+            throw new NotSupportedException();
         }
 
-        public static Task<int> ReadAsync(this ISocket socket, Memory<byte> buffer)
+        public static Task<int> ReadAsync(this ISocket socket, Memory<byte> buffer, CancellationToken token = default)
         {
-            throw new Exception();
+            if (MemoryMarshal.TryGetArray<byte>(buffer, out var segment))
+            {
+                return socket.Stream.ReadAsync(segment.Array, segment.Offset, segment.Count);
+            }
+
+            throw new NotSupportedException();
         }
 
         public static void Write(this ISocket socket, ReadOnlyMemory<byte> buffer)
         {
-            throw new Exception();
+            if (MemoryMarshal.TryGetArray(buffer, out var segment))
+            {
+                socket.Stream.Write(segment.Array, segment.Offset, segment.Count);
+            }
+
+            throw new NotSupportedException();
         }
 
-        public static Task WriteAsync(this ISocket socket, ReadOnlyMemory<byte> buffer)
+        public static Task WriteAsync(this ISocket socket, ReadOnlyMemory<byte> buffer, CancellationToken token = default)
         {
-            throw new Exception();
+            if (MemoryMarshal.TryGetArray(buffer, out var segment))
+            {
+                return socket.Stream.WriteAsync(segment.Array, segment.Offset, segment.Count);
+            }
+
+            throw new NotSupportedException();
         }
     }
 }
--- test/HyperMsg.Socket.Tests/SocketExtensionTests.cs
+++ test/HyperMsg.Socket.Tests/SocketExtensionTests.cs
@@ -2,6 +2,7 @@
 using HyperMsg.Sockets;
 using System;
 using System.IO;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace HyperMsg.Socket.Tests
@@ -21,6 +22,49 @@ namespace HyperMsg.Socket.Tests
 
             Assert.Equal(bytes.Length, readed);
             Assert.Equal(bytes, actualBytes);
+        }
+
+        [Fact]
+        public async Task ReadAsync_Reads_Bytes_From_Stream_Into_Buffer()
+        {
+            var bytes = Guid.NewGuid().ToByteArray();
+            var stream = new MemoryStream(bytes);
+            var socket = A.Fake<ISocket>();
+            A.CallTo(() => socket.Stream).Returns(stream);
+            var actualBytes = new byte[bytes.Length];
+
+            int readed = await socket.ReadAsync(actualBytes);
+
+            Assert.Equal(bytes.Length, readed);
+            Assert.Equal(bytes, actualBytes);
+        }
+
+        //[Fact]
+        public void Write_Writes_Bytes_Into_Stream()
+        {
+            var bytes = Guid.NewGuid().ToByteArray();
+            var stream = new MemoryStream();
+            var socket = A.Fake<ISocket>();
+            A.CallTo(() => socket.Stream).Returns(stream);
+            
+            socket.Write(bytes);
+
+            var actualBytes = stream.ToArray();
+
+            Assert.Equal(bytes, actualBytes);
+        }
+
+        [Fact]
+        public async Task WriteAsync_Writes_Bytes_Into_Stream()
+        {
+            var bytes = Guid.NewGuid().ToByteArray();
+            var stream = new MemoryStream();
+            var socket = A.Fake<ISocket>();
+            A.CallTo(() => socket.Stream).Returns(stream);
+
+            await socket.WriteAsync(bytes);
+                        
+            Assert.Equal(bytes, stream.ToArray());
         }
     }
 }
