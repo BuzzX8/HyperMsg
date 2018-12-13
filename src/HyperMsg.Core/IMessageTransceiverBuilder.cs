5629f8dae04b1831b2a020bdc72e3b8aab43384e
Maxim Chasnik <maksym.c@sbtech.com>
2018-12-13 18:35:20 +0200

    Added BuilderContext

--- src/HyperMsg.Core/IMessageTransceiverBuilder.cs
+++ src/HyperMsg.Core/IMessageTransceiverBuilder.cs
@@ -1,7 +1,11 @@
-﻿namespace HyperMsg
+﻿using System;
+
+namespace HyperMsg
 {
     public interface IMessageTransceiverBuilder<T>
     {
+        void Configure(Action<BuilderContext> configurator);
+
         IMessageTransceiver<T> Build();
     }
 }
