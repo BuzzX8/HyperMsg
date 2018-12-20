2cd789a5eff851471c8590376987651dc0a38263
Maxim Chasnik <maxim.chasnik@outlook.com>
2018-12-20 20:18:36 +0200

    Introduced pipe interface

---
+++ src/HyperMsg.Core/IPipe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Buffers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg
+{
+    public interface IPipe
+    {
+        IPipeReader Reader { get; }
+
+        IPipeWriter Writer { get; }
+    }
+
+    public interface IPipeReader
+    {
+        ReadOnlySequence<byte> Read();
+
+        Task<ReadOnlySequence<byte>> ReadAsync(CancellationToken token = default);
+
+        void Advance(int length);
+    }
+
+    public interface IPipeWriter
+    {
+        Memory<byte> GetMemory(int sizeHint);
+
+        void Flush();
+
+        Task FlushAsync(CancellationToken token = default);
+    }
+}
