7c9969ee7818dbaf014130bfe1effbd3abe166cb
Maxim Chasnik <maxim.chasnik@gmail.com>
2019-01-06 21:05:35 +0200

    extended builder context (#18)

---
+++ src/HyperMsg.Transciever/MessageBuffer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Buffers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg.Transciever
+{
+    class MessageBuffer<T> : IMessageBuffer<T>
+    {
+        private readonly IPipeWriter writer;
+        private readonly Action<IBufferWriter<byte>, T> serializeAction;
+
+        public MessageBuffer(IPipeWriter writer, Action<IBufferWriter<byte>, T> serializeAction)
+        {
+            this.writer = writer ?? throw new ArgumentNullException(nameof(writer));
+            this.serializeAction = serializeAction ?? throw new ArgumentNullException(nameof(serializeAction));
+        }
+
+        public Task FlushAsync(CancellationToken token = default) => writer.FlushAsync(token);
+
+        public void Write(T message) => serializeAction.Invoke(writer, message);
+    }
+}
