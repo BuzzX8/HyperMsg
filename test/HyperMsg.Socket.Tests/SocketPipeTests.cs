f673084a83031a1ff77ee86621498a1efcc9ee8a
Maxim Chasnik <maxim.chasnik@outlook.com>
2019-01-20 15:37:45 +0200

    Clean up

--- test/HyperMsg.Socket.Tests/SocketPipeTests.cs
+++ test/HyperMsg.Socket.Tests/SocketPipeTests.cs
@@ -18,33 +18,7 @@ namespace HyperMsg.Socket.Tests
         {
             pipe = A.Fake<IPipe>();
             socket = A.Fake<ISocket>();
-            socketPipe = new SocketPipe(pipe, socket);
-        }
-
-        [Fact]
-        public async Task TransferFromSocketAsync_Reads_Data_From_Socket_And_Writes_Into_Writer()
-        {
-            var data = Guid.NewGuid().ToByteArray();
-            var buffer = new byte[data.Length];
-            A.CallTo(() => pipe.Writer.GetMemory(A<int>._)).Returns(new Memory<byte>(buffer));
-            A.CallTo(() => socket.Stream).Returns(new MemoryStream(data));
-
-            await socketPipe.TransferFromSocketAsync(default);
-
-            Assert.Equal(data, buffer);
-        }
-
-        [Fact]
-        public async Task TransferToSocketAsync_Reads_Data_From_Reader_And_Writes_Into_Socket()
-        {
-            var data = Guid.NewGuid().ToByteArray();
-            var stream = new MemoryStream();
-            A.CallTo(() => socket.Stream).Returns(stream);
-            A.CallTo(() => pipe.Reader.ReadAsync(default)).Returns(Task.FromResult(new ReadOnlySequence<byte>(data)));
-
-            await socketPipe.TransferToSocketAsync(default);
-
-            Assert.Equal(data, stream.ToArray());
+            socketPipe = new SocketPipe(socket);
         }
     }
 }
