c17c56d4100d0fca8d1654dab80ed856190e1fbe
Maxim Chasnik <maxim.chasnik@gmail.com>
2019-01-26 10:47:49 +0200

    Refinement (#20)
    * namespace adjustment
    
    * Added tests for MessageTransciever
    
    * Refactored message part
    
    * wip
    
    * Clean up
    
    * more refactorings
    
    * Added tests for transciever
    
    * fixed tests for builder

--- test/HyperMsg.Core.Tests/MessageTransceiverTests.cs
+++ test/HyperMsg.Core.Tests/MessageTransceiverTests.cs
@@ -1,6 +1,7 @@
 ﻿using FakeItEasy;
 using System;
 using System.Buffers;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -8,15 +9,19 @@ namespace HyperMsg.Transciever
 {
     public class MessageTransceiverTests
     {
-        private readonly IMessageBuffer<Guid> messageBuffer;
-        private readonly MessageTransceiver<Guid> transceiver;
-        private readonly IObservable<Guid> observer;
+        private readonly IMessageBuffer<Guid> messageBuffer;        
+        private readonly ISubject<Guid> subject;
+        private readonly MessageReceiverFactory<Guid> messageReceiverFactory;
+        private MessageTransceiver<Guid> transceiver;
+        private List<Func<IDisposable>> runners;
 
         public MessageTransceiverTests()
         {
             messageBuffer = A.Fake<IMessageBuffer<Guid>>();
-            observer = A.Fake<IObservable<Guid>>();
-            transceiver = new MessageTransceiver<Guid>(messageBuffer, observer);
+            subject = A.Fake<ISubject<Guid>>();
+            messageReceiverFactory = A.Fake<MessageReceiverFactory<Guid>>();
+            runners = new List<Func<IDisposable>>(A.CollectionOfFake<Func<IDisposable>>(10));
+            transceiver = new MessageTransceiver<Guid>(messageBuffer, messageReceiverFactory, subject, runners);
         }
 
         [Fact]
@@ -37,6 +42,50 @@ namespace HyperMsg.Transciever
             await transceiver.SendAsync(message);
 
             A.CallTo(() => messageBuffer.Write(message)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void Run_Invokes_All_Child_Runners()
+        {
+            transceiver.Run();
+
+            foreach(var runner in runners)
+            {
+                A.CallTo(() => runner.Invoke()).MustHaveHappened();
+            }
+        }
+
+        [Fact]
+        public void Subscribe_Calls_MessageSubject_Subscribe()
+        {
+            var disposable = A.Fake<IDisposable>();
+            A.CallTo(() => subject.Subscribe(A<IObserver<Guid>>._)).Returns(disposable);
+            var observer = A.Fake<IObserver<Guid>>();
+
+            var actual = transceiver.Subscribe(observer);
+
+            Assert.Same(disposable, actual);
+            A.CallTo(() => subject.Subscribe(observer)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void ReadBuffer_Invokes_ReadBufferAction()
+        {
+            var message = Guid.NewGuid();
+            var onMessage = (Action<Guid>)null;
+            A.CallTo(() => messageReceiverFactory.Invoke(A<Action<Guid>>._)).Invokes(foc =>
+            {
+                onMessage = foc.GetArgument<Action<Guid>>(0);
+            })
+            .Returns(b =>
+            {
+                onMessage(message);
+                return -1;
+            });
+
+            var readed = transceiver.ReadBuffer(new ReadOnlySequence<byte>());
+
+            A.CallTo(() => subject.OnNext(message)).MustHaveHappened();
         }
     }
 }
--- test/HyperMsg.Core.Tests/TranscieverBuilderTests.cs
+++ test/HyperMsg.Core.Tests/TranscieverBuilderTests.cs
@@ -1,9 +1,8 @@
 ﻿using FakeItEasy;
-using HyperMsg.Transciever;
 using System;
 using Xunit;
 
-namespace HyperMsg
+namespace HyperMsg.Transciever
 {
     public class TranscieverBuilderTests
     {
@@ -11,7 +10,11 @@ namespace HyperMsg
         public void Build_Run_All_Configurators()
         {
             var serviceProvider = A.Fake<IServiceProvider>();
+            A.CallTo(() => serviceProvider.GetService(typeof(IPipeWriter))).Returns(A.Fake<IPipeWriter>());
+            A.CallTo(() => serviceProvider.GetService(typeof(ISerializer<Guid>))).Returns(A.Fake<ISerializer<Guid>>());
+            A.CallTo(() => serviceProvider.GetService(typeof(ISubject<Guid>))).Returns(A.Fake<ISubject<Guid>>());
             A.CallTo(() => serviceProvider.GetService(typeof(ITransceiver<Guid, Guid>))).Returns(A.Fake<ITransceiver<Guid, Guid>>());
+            A.CallTo(() => serviceProvider.GetService(typeof(MessageReceiverFactory<Guid>))).Returns(A.Fake<MessageReceiverFactory<Guid>>());
             var builder = new TranscieverBuilder<Guid>(d => serviceProvider);
 
             var configurators = A.CollectionOfFake<Action<BuilderContext>>(10);
