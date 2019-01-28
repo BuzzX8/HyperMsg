2835d4dccee71e5e98609b15e80a63773725af3d
Maxim Chasnik <maxim.chasnik@outlook.com>
2019-01-28 20:49:53 +0200

    Added MessageReader

---
+++ src/HyperMsg.Transciever/MessageReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Buffers;
+
+namespace HyperMsg.Transciever
+{
+    public class MessageReader<T>
+    {
+        private readonly Func<ReadOnlySequence<byte>, DeserializationResult<T>> deserializeFunc;
+        private Action<T> messageHandler;
+
+        public MessageReader(Func<ReadOnlySequence<byte>, DeserializationResult<T>> deserializeFunc)
+        {
+            this.deserializeFunc = deserializeFunc ?? throw new ArgumentNullException(nameof(deserializeFunc));
+            messageHandler = null;
+        }
+
+        public int ReadBuffer(ReadOnlySequence<byte> buffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetMessageHandler(Action<T> handler)
+        {
+            messageHandler = handler;
+        }
+    }
+}
