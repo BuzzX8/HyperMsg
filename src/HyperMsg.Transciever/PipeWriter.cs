15f1bf21913f6e80f41d0a68a3139a72a65e3f13
Maxim Chasnik <maxim.chasnik@outlook.com>
2019-02-03 18:42:00 +0200

    Added Pipe classes

---
+++ src/HyperMsg.Transciever/PipeWriter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Buffers;
+using System.Threading;
+using System.Threading.Tasks;
+using BufferReader = System.Func<System.Buffers.ReadOnlySequence<byte>, int>;
+
+namespace HyperMsg.Transciever
+{
+    public class PipeWriter : IPipeWriter
+    {
+        private readonly IMemoryOwner<byte> memoryOwner;
+        private readonly BufferReader bufferReader;
+        private readonly SequencePosition position;
+
+        public PipeWriter(IMemoryOwner<byte> memoryOwner, BufferReader bufferReader)
+        {
+            this.memoryOwner = memoryOwner ?? throw new ArgumentNullException(nameof(memoryOwner));
+            this.bufferReader = bufferReader ?? throw new ArgumentNullException(nameof(bufferReader));
+            position = new SequencePosition(memoryOwner.Memory, 0);
+        }
+
+        private Memory<byte> Memory => memoryOwner.Memory;
+
+        public void Advance(int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task FlushAsync(CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Memory<byte> GetMemory(int sizeHint = 0)
+        {
+            if (Memory.Length < sizeHint)
+            {
+
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public Span<byte> GetSpan(int sizeHint = 0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/HyperMsg.Core.Tests/PipeWriterTests.cs
@@ -0,0 +1,22 @@
+﻿using FakeItEasy;
+using System;
+using System.Buffers;
+using Xunit;
+
+namespace HyperMsg.Transciever
+{
+    public class PipeWriterTests
+    {
+        [Fact]
+        public void Flush_Calls_BufferReader()
+        {
+            var owner = A.Fake<IMemoryOwner<byte>>();
+            var reader = A.Fake<Func<ReadOnlySequence<byte>, int>>();
+            var writer = new PipeWriter(owner, reader);
+
+            writer.Flush();
+
+            A.CallTo(() => reader.Invoke(A<ReadOnlySequence<byte>>._)).MustHaveHappened();
+        }
+    }
+}
