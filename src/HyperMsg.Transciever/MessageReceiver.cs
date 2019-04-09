9e70d044cb30d3c9ead60b6cf6a2bdf9ae9e8818
Maxim Chasnik <maksym.c@sbtech.com>
2019-04-09 11:30:24 +0300

    Added MessageReceiver

---
+++ src/HyperMsg.Transciever/MessageReceiver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Buffers;
+using System.Threading;
+using System.Threading.Tasks;
+using ReadAsyncFunc = System.Func<System.Memory<byte>, System.Threading.CancellationToken, System.Threading.Tasks.Task<int>>;
+
+namespace HyperMsg.Transciever
+{
+    public class MessageReceiver<T> : IReceiver<T>
+    {
+        private readonly DeserializeFunc<T> deserialize;
+        private readonly Memory<byte> buffer;
+        private readonly ReadAsyncFunc readAsync;
+
+        public MessageReceiver(DeserializeFunc<T> deserialize, Memory<byte> buffer, ReadAsyncFunc readAsync)
+        {
+            this.deserialize = deserialize ?? throw new ArgumentNullException(nameof(deserialize));
+            this.buffer = buffer;
+            this.readAsync = readAsync ?? throw new ArgumentNullException(nameof(readAsync));
+        }
+
+        public T Receive() => ReceiveAsync(CancellationToken.None).GetAwaiter().GetResult();
+
+        public async Task<T> ReceiveAsync(CancellationToken token)
+        {
+            var readed = await readAsync.Invoke(buffer, token);
+            var result = deserialize.Invoke(new ReadOnlySequence<byte>(buffer.Slice(0, readed)));
+
+            if (result.BytesConsumed > 0)
+            {
+                return result.Message;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/HyperMsg.Core.Tests/MessageReceiverTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Buffers;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HyperMsg.Transciever
+{
+    public class MessageReceiverTests
+    {
+        [Fact]
+        public void Receive_Returns_Fully_Received_Message()
+        {
+            var expectedMessage = Guid.NewGuid();
+            var buffer = new Memory<byte>(new byte[100]);
+            var receiver = new MessageReceiver<Guid>(DeserializeGuid, buffer, (b, t) =>
+            {
+                var bytes = expectedMessage.ToByteArray();
+                bytes.CopyTo(buffer);
+                return Task.FromResult(bytes.Length);
+            });
+
+            var acctualMessage = receiver.Receive();
+
+            Assert.Equal(expectedMessage, acctualMessage);
+        }
+
+        private DeserializationResult<Guid> DeserializeGuid(ReadOnlySequence<byte> buffer)
+        {
+            var bytes = buffer.ToArray();
+            return new DeserializationResult<Guid>(bytes.Length, new Guid(bytes));
+        }
+    }
+}
