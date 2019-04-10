52ecb78713a080a0a801a67ab2cc1782475f1bb1
Maxim Chasnik <maksym.c@sbtech.com>
2019-04-10 14:01:07 +0300

    cleaned transciever

--- test/HyperMsg.Core.Tests/MessageTransceiverTests.cs
+++ test/HyperMsg.Core.Tests/MessageTransceiverTests.cs
@@ -1,55 +1,16 @@
-﻿using FakeItEasy;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Xunit;
+﻿using System;
 
 namespace HyperMsg
 {
     public class MessageTransceiverTests
     {
-        private readonly IMessageBuffer<Guid> messageBuffer;
-        private readonly SetHandlerAction<Guid> setHandlerAction;
-        private MessageTransceiver<Guid> transceiver;
-        private List<Func<IDisposable>> runners;
+        private readonly ISerializer<Guid> serializer;
+        private readonly IStream stream;
+        private readonly MessageTransceiver<Guid> transceiver;
 
         public MessageTransceiverTests()
         {
-            messageBuffer = A.Fake<IMessageBuffer<Guid>>();
-            runners = new List<Func<IDisposable>>(A.CollectionOfFake<Func<IDisposable>>(10));
-            setHandlerAction = A.Fake<SetHandlerAction<Guid>>();
-            transceiver = new MessageTransceiver<Guid>(messageBuffer, setHandlerAction, runners);
-        }
-
-        [Fact]
-        public void Send_Serializes_Message()
-        {
-            var message = Guid.NewGuid();
-
-            transceiver.Send(message);
-
-            A.CallTo(() => messageBuffer.Write(message)).MustHaveHappened();
-        }
-
-        [Fact]
-        public async Task SendAsync_Serializes_Message()
-        {
-            var message = Guid.NewGuid();
-
-            await transceiver.SendAsync(message);
-
-            A.CallTo(() => messageBuffer.Write(message)).MustHaveHappened();
-        }
-
-        [Fact]
-        public void Run_Invokes_All_Child_Runners()
-        {
-            transceiver.Run();
-
-            foreach(var runner in runners)
-            {
-                A.CallTo(() => runner.Invoke()).MustHaveHappened();
-            }
+            
         }
     }
 }
