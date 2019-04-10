60f03ea14479cdd3f3fc1bc9e314ea250aa77c8a
Maxim Chasnik <maksym.c@sbtech.com>
2019-04-10 14:56:00 +0300

    Rewrite cases for transciever

--- src/HyperMsg.Core/MessageTransceiver.cs
+++ src/HyperMsg.Core/MessageTransceiver.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,18 +9,15 @@ namespace HyperMsg
         private readonly MessageBuffer<T> messageBuffer;
         private readonly MessageReceiver<T> messageReceiver;
 
-        public MessageTransceiver(ISerializer<T> serializer, IStream stream)
+        public MessageTransceiver(ISerializer<T> serializer, Memory<byte> sendBuffer, Memory<byte> receiveBuffer, IStream stream)
         {
             //messageBuffer = new MessageBuffer<T>(null, null, serializer.Serialize);
-            messageReceiver = new MessageReceiver<T>(serializer.Deserialize, new Memory<byte>(), stream.ReadAsync);
+            messageReceiver = new MessageReceiver<T>(serializer.Deserialize, receiveBuffer, stream.ReadAsync);
         }
 
-        public void Send(T message) => SendAsync(message).GetAwaiter().GetResult();
+        public void Send(T message) => messageBuffer.Send(message);
 
-        public async Task SendAsync(T message, CancellationToken token = default)
-        {
-            throw new NotImplementedException();
-        }
+        public Task SendAsync(T message, CancellationToken token = default) => messageBuffer.SendAsync(message, token);
 
         public T Receive() => messageReceiver.Receive();
 
--- test/HyperMsg.Core.Tests/MessageTransceiverTests.cs
+++ test/HyperMsg.Core.Tests/MessageTransceiverTests.cs
@@ -1,16 +1,76 @@
-﻿using System;
+﻿using FakeItEasy;
+using System;
+using System.Buffers;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace HyperMsg
 {
     public class MessageTransceiverTests
     {
         private readonly ISerializer<Guid> serializer;
+        private readonly Memory<byte> receiveBuffer;
+        private readonly Memory<byte> sendBuffer;
         private readonly IStream stream;
         private readonly MessageTransceiver<Guid> transceiver;
 
+        const int GuidSize = 16;
+
         public MessageTransceiverTests()
         {
-            
+            serializer = A.Fake<ISerializer<Guid>>();
+            stream = A.Fake<IStream>();
+            receiveBuffer = new Memory<byte>(new byte[100]);
+            sendBuffer = new Memory<byte>(new byte[100]);
+
+            transceiver = new MessageTransceiver<Guid>(serializer, sendBuffer, receiveBuffer, stream);
+        }
+
+        [Fact]
+        public void Send_Writes_Serialized_Message_Into_Stream()
+        {
+            var message = Guid.NewGuid();
+
+            transceiver.Send(message);
+
+            A.CallTo(() => serializer.Serialize(A<IBufferWriter<byte>>._, message)).MustHaveHappened();
+            A.CallTo(() => stream.Write(A<Memory<byte>>._)).MustHaveHappened();
+        }
+
+        [Fact]
+        public async Task SendAsync_Writes_Serialized_Message_Into_Stream()
+        {
+            var message = Guid.NewGuid();
+
+            await transceiver.SendAsync(message);
+
+            A.CallTo(() => serializer.Serialize(A<IBufferWriter<byte>>._, message)).MustHaveHappened();
+            A.CallTo(() => stream.Write(A<Memory<byte>>._)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void Receive_Returns_Message_From_DeserializationResult()
+        {
+            var expectedMessage = Guid.NewGuid();
+            A.CallTo(() => serializer.Deserialize(A<ReadOnlySequence<byte>>._)).Returns(new DeserializationResult<Guid>(GuidSize, expectedMessage));
+            A.CallTo(() => stream.ReadAsync(A<Memory<byte>>._, A<CancellationToken>._)).Returns(Task.FromResult(GuidSize));
+
+            var actualMessage = transceiver.Receive();
+
+            Assert.Equal(expectedMessage, actualMessage);
+        }
+
+        [Fact]
+        public async Task ReceiveAsync_Returns_Message_From_DeserializationResult()
+        {
+            var expectedMessage = Guid.NewGuid();
+            A.CallTo(() => serializer.Deserialize(A<ReadOnlySequence<byte>>._)).Returns(new DeserializationResult<Guid>(GuidSize, expectedMessage));
+            A.CallTo(() => stream.ReadAsync(A<Memory<byte>>._, A<CancellationToken>._)).Returns(Task.FromResult(GuidSize));
+
+            var actualMessage = await transceiver.ReceiveAsync();
+
+            Assert.Equal(expectedMessage, actualMessage);
         }
     }
 }
