37aaeac2fbc344f8be4027a622e6838559e0f749
Faceless <maxim.chasnik@gmail.com>
2019-04-10 14:56:59 +0300

    Transciever (#27)
    * cleaned transciever
    
    * Rewrite cases for transciever

--- src/HyperMsg.Core/MessageTransceiver.cs
+++ src/HyperMsg.Core/MessageTransceiver.cs
@@ -1,74 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace HyperMsg
 {
-    public class MessageTransceiver<T> : ITransceiver<T, T>, IDisposable
+    public class MessageTransceiver<T> : ITransceiver<T, T>
     {
-        private readonly IMessageBuffer<T> messageBuffer;
-        private readonly List<Func<IDisposable>> runners;
-        private List<IDisposable> handlers;
+        private readonly MessageBuffer<T> messageBuffer;
+        private readonly MessageReceiver<T> messageReceiver;
 
-        public MessageTransceiver(IMessageBuffer<T> messageBuffer, SetHandlerAction<T> setHandlerAction, ICollection<Func<IDisposable>> runners)
+        public MessageTransceiver(ISerializer<T> serializer, Memory<byte> sendBuffer, Memory<byte> receiveBuffer, IStream stream)
         {
-            this.messageBuffer = messageBuffer ?? throw new ArgumentNullException(nameof(messageBuffer));
-            this.runners = new List<Func<IDisposable>>();
-            this.runners.AddRange(runners);
-            handlers = new List<IDisposable>();
-        }        
-
-        public IDisposable Run()
-        {
-            RunChildRunnersIfRequired();
-            return this;
+            //messageBuffer = new MessageBuffer<T>(null, null, serializer.Serialize);
+            messageReceiver = new MessageReceiver<T>(serializer.Deserialize, receiveBuffer, stream.ReadAsync);
         }
 
-        public void Send(T message) => SendAsync(message).GetAwaiter().GetResult();
+        public void Send(T message) => messageBuffer.Send(message);
 
-        public async Task SendAsync(T message, CancellationToken token = default)
-        {
-            messageBuffer.Write(message);
-            await messageBuffer.FlushAsync(token);
-        }
+        public Task SendAsync(T message, CancellationToken token = default) => messageBuffer.SendAsync(message, token);
 
-        public void SetMessageHandler(Action<T> handler)
-        { }
+        public T Receive() => messageReceiver.Receive();
 
-        private void RunChildRunnersIfRequired()
-        {
-            if (handlers.Any())
-            {
-                return;
-            }
-
-            foreach (var runner in runners)
-            {
-                var handle = runner.Invoke();
-                handlers.Add(handle);
-            }
-        }
-
-        public void Dispose()
-        {
-            foreach (var handler in handlers)
-            {
-                handler.Dispose();
-            }
-        }
-
-        public T Receive()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<T> ReceiveAsync(CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<T> ReceiveAsync(CancellationToken token = default) => messageReceiver.ReceiveAsync(token);
     }
-
-    public delegate void SetHandlerAction<T>(Action<T> handler);
 }
