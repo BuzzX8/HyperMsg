fea6dedf0f5ac35370dfc813848cffa78ef42de0
Faceless <maxim.chasnik@gmail.com>
2019-04-10 12:46:30 +0300

    IStream

---
+++ src/HyperMsg.Sockets/SocketStream.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg.Sockets
+{
+    public class SocketStream : IStream
+    {
+        private readonly ISocket socket;
+
+        public SocketStream(ISocket socket)
+        {
+            this.socket = socket ?? throw new ArgumentNullException(nameof(socket));
+        }
+
+        public int Read(Memory<byte> buffer) => socket.Read(buffer);
+
+        public Task<int> ReadAsync(Memory<byte> buffer, CancellationToken token = default) => socket.ReadAsync(buffer, token);
+
+        public void Write(Memory<byte> buffer) => socket.Write(buffer);
+
+        public Task WriteAsync(Memory<byte> buffer, CancellationToken token = default) => socket.WriteAsync(buffer, token);
+    }
+}
