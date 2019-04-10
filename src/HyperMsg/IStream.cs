c3540d7aa4d89d402eec781cf196e83dc07623b5
Maxim Chasnik <maksym.c@sbtech.com>
2019-04-10 12:44:56 +0300

    Added IStream, SocketStream

---
+++ src/HyperMsg/IStream.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IStream
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        int Read(Memory<byte> buffer);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        Task<int> ReadAsync(Memory<byte> buffer, CancellationToken token = default);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="buffer"></param>
+        void Write(Memory<byte> buffer);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        Task WriteAsync(Memory<byte> buffer, CancellationToken token = default);
+    }
+}
