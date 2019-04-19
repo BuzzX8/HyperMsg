f18a96ccd044b3782ce2dd20ef97de964986a953
Max Gunther <maxim.chasnik@gmail.com>
2019-04-19 15:22:00 +0300

    BgReceiver (#31)

---
+++ src/HyperMsg/RunHandlingFunc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HyperMsg
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="handler"></param>
+    /// <returns></returns>
+    public delegate IDisposable RunHandlingFunc<T>(Action<T> handler);
+}
