c739e9b3eeb6dd3713c663fc83c84dd3e39cb4dd
Maxim Chasnik <maksym.c@sbtech.com>
2019-04-19 13:29:31 +0300

    Added BgReceiver

---
+++ src/HyperMsg.Core/BackgroundReceiver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HyperMsg
+{
+    public class BackgroundReceiver<T> : IDisposable
+    {
+        private readonly IReceiver<T> messageReceiver;
+        private Action<T> messageHandler;
+
+        public BackgroundReceiver(IReceiver<T> messageReceiver)
+        {
+            this.messageReceiver = messageReceiver ?? throw new ArgumentNullException(nameof(messageReceiver));
+        }
+
+        public IDisposable Run(Action<T> messageHandler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        { }
+
+        public Action<Exception> OnUnhandlerException;
+    }
+}
---
+++ test/HyperMsg.Core.Tests/BackgroundReceiverTests.cs
@@ -0,0 +1,17 @@
+﻿using FakeItEasy;
+using System;
+
+namespace HyperMsg.Transciever
+{
+    public class BackgroundReceiverTests
+    {
+        private readonly IReceiver<Guid> messageReceiver;
+        private readonly BackgroundReceiver<Guid> backgroundReceiver;
+
+        public BackgroundReceiverTests()
+        {
+            messageReceiver = A.Fake<IReceiver<Guid>>();
+            backgroundReceiver = new BackgroundReceiver<Guid>(messageReceiver);
+        }
+    }
+}
