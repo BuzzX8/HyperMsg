035279d7937147a58eed07f8b0a90e022075ee01
Maxim Chasnik <maksym.c@sbtech.com>
2019-04-19 15:03:37 +0300

    Added tests for BG receiver

--- src/HyperMsg.Core/BackgroundReceiver.cs
+++ src/HyperMsg.Core/BackgroundReceiver.cs
@@ -1,24 +1,64 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HyperMsg
 {
     public class BackgroundReceiver<T> : IDisposable
     {
         private readonly IReceiver<T> messageReceiver;
-        private Action<T> messageHandler;
+
+        private CancellationTokenSource tokenSource;
+        private Action<T> messageHandler;        
+        private Task backgroundTask;
+        private bool tokenSourceDisposed;
 
         public BackgroundReceiver(IReceiver<T> messageReceiver)
         {
             this.messageReceiver = messageReceiver ?? throw new ArgumentNullException(nameof(messageReceiver));
+            backgroundTask = Task.CompletedTask;
         }
 
         public IDisposable Run(Action<T> messageHandler)
         {
-            throw new NotImplementedException();
+            this.messageHandler = messageHandler ?? throw new ArgumentNullException(nameof(messageHandler));
+            tokenSource = new CancellationTokenSource();
+            backgroundTask = RunBackgroundTask(tokenSource.Token);
+
+            return this;
         }
 
         public void Dispose()
-        { }
+        {
+            if (tokenSourceDisposed)
+            {
+                return;
+            }
+
+            tokenSource?.Cancel();
+            tokenSource?.Dispose();
+            tokenSourceDisposed = true;
+        }
+
+        private Task RunBackgroundTask(CancellationToken token)
+        {
+            return Task.Run(() => DoWorkAsync(token), tokenSource.Token).ContinueWith(OnBackgroundTaskFault, TaskContinuationOptions.OnlyOnFaulted);            
+        }
+
+        private async Task DoWorkAsync(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var message = await messageReceiver.ReceiveAsync(token);
+                messageHandler.Invoke(message);
+            }
+        }
+
+        private void OnBackgroundTaskFault(Task task)
+        {
+            var exception = task.Exception.Flatten()?.InnerException;
+            OnUnhandlerException?.Invoke(exception);
+        }
 
         public Action<Exception> OnUnhandlerException;
     }
--- test/HyperMsg.Core.Tests/BackgroundReceiverTests.cs
+++ test/HyperMsg.Core.Tests/BackgroundReceiverTests.cs
@@ -1,17 +1,91 @@
 ﻿using FakeItEasy;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
 
-namespace HyperMsg.Transciever
+namespace HyperMsg
 {
-    public class BackgroundReceiverTests
+    public class BackgroundReceiverTests : IDisposable
     {
         private readonly IReceiver<Guid> messageReceiver;
         private readonly BackgroundReceiver<Guid> backgroundReceiver;
 
+        private readonly TimeSpan waitTimeout = TimeSpan.FromSeconds(2);
+
         public BackgroundReceiverTests()
         {
             messageReceiver = A.Fake<IReceiver<Guid>>();
-            backgroundReceiver = new BackgroundReceiver<Guid>(messageReceiver);
+            backgroundReceiver = new BackgroundReceiver<Guid>(messageReceiver);            
+        }
+
+        [Fact]
+        public void Run_Invokes_Message_Handler()
+        {
+            var expected = Guid.NewGuid();
+            var actual = Guid.Empty;            
+            var @event = new ManualResetEventSlim();
+            A.CallTo(() => messageReceiver.ReceiveAsync(A<CancellationToken>._)).Returns(Task.FromResult(expected));
+                        
+            backgroundReceiver.Run(m =>
+            {
+                actual = m;
+                @event.Set();
+            });            
+            @event.Wait(waitTimeout);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Dispose_Stops_Background_Task()
+        {
+            var @event = new ManualResetEventSlim();
+            var @event2 = new ManualResetEventSlim();
+            var wasInvoked = false;
+
+            var disp = backgroundReceiver.Run(m =>
+            {
+                @event.Wait();
+                wasInvoked = true;
+                event2.Set();
+            });
+            Assert.Same(backgroundReceiver, disp);
+
+            backgroundReceiver.Dispose();
+            @event.Set();
+            event2.Wait(waitTimeout);
+
+            Assert.False(wasInvoked);
+        }
+
+        [Fact(Skip = "TODO: ")]
+        public void OnUnhandledException_Rises_With_Correct_Exception()
+        {
+            var expected = new ArgumentNullException();
+            var actual = (Exception)null;
+            backgroundReceiver.OnUnhandlerException += e => actual = e;
+            var @event = new ManualResetEventSlim();
+
+            backgroundReceiver.Run(m =>
+            {
+                try
+                {
+                    throw expected;
+                }
+                finally
+                {
+                    @event.Set();
+                }
+            });
+            @event.Wait(waitTimeout);
+
+            Assert.Equal(expected, actual);
+        }
+
+        public void Dispose()
+        {
+            backgroundReceiver.Dispose();
         }
     }
 }
