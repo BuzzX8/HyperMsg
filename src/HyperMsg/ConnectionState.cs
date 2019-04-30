c38ba60d20303251f52426bf802260ec2f3d3dc7
Max Gunther <maxim.chasnik@gmail.com>
2019-04-30 18:07:07 +0300

    Connection (#40)

---
+++ src/HyperMsg/ConnectionState.cs
@@ -0,0 +1,11 @@
+﻿namespace HyperMsg
+{
+    public enum ConnectionState
+    {
+        None,
+        Opening,
+        Open,
+        Closing,
+        Closed
+    }
+}
---
+++ src/HyperMsg/IConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg
+{
+    public interface IConnection
+    {
+        ConnectionState State { get; }
+
+        void Open();
+
+        Task OpenAsync(CancellationToken token = default);
+
+        void Close();
+
+        Task CloseAsync(CancellationToken token = default);
+
+        event EventHandler<ConnectionStateChangedEventArgs> StateChanged;
+    }
+}
