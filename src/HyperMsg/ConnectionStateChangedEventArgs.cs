26110ab9b8a55a5c313446d9e3328e4be4e7ac83
BuzzX8 <maxim.chasnik@gmail.com>
2019-04-30 17:46:44 +0300

    wip

---
+++ src/HyperMsg/ConnectionStateChangedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HyperMsg
+{
+    public class ConnectionStateChangedEventArgs : EventArgs
+    {
+        public ConnectionStateChangedEventArgs(ConnectionState state)
+        {
+            State = state;
+        }
+
+        public ConnectionState State { get; }
+    }
+}
