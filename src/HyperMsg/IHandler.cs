c4f7219afcbd596ac03534d6fab062e8f3b12e70
Max Gunther <maxim.chasnik@gmail.com>
2019-05-08 09:08:15 +0300

    CommandHandler (#41)
    * Added interfaces for command handler
    
    * removed connection and friends
    
    * renamed handler
    
    * changed sockets for handling transport commands

---
+++ src/HyperMsg/IHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IHandler
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="message"></param>
+        void Handle<T>(T message);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="message"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        Task HandleAsync<T>(T message, CancellationToken token = default);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IHandler<T>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        void Handle(T message);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        Task HandleAsync(T message, CancellationToken token = default);
+    }    
+}
