cf49383494156c7b1de111a31ff69716b80f9d04
BuzzX8 <maksym.chasnik@gmail.com>
2019-05-22 15:38:11 +0300

    Added SetTransportSecurity for SocketProxy

---
+++ src/HyperMsg.Sockets/RemoteCertificateValidationEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HyperMsg.Sockets
+{
+    public class RemoteCertificateValidationEventArgs : EventArgs
+    {
+        public RemoteCertificateValidationEventArgs(X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            Certificate = certificate ?? throw new ArgumentNullException(nameof(certificate));
+            Chain = chain ?? throw new ArgumentNullException(nameof(chain));
+            SslPolicyErrors = sslPolicyErrors;
+        }
+
+        public X509Certificate Certificate { get; }
+
+        public X509Chain Chain { get; }
+
+        public SslPolicyErrors SslPolicyErrors { get; }
+
+        public bool IsValid { get; set; }
+    }
+}
