e4d836df9211e624add73eb0c230e1a3e30f1e5d
BuzzX8 <maksym.chasnik@gmail.com>
2019-05-22 14:04:02 +0300

    Added more config extensions

--- src/HyperMsg.Core/ConfigurableExtensions.cs
+++ src/HyperMsg.Core/ConfigurableExtensions.cs
@@ -13,5 +13,46 @@
                 context.RegisterService(typeof(ITransceiver<T, T>), transciever);
             });
         }
+
+        public static void UseMessageBuffer<T>(this IConfigurable configurable, int bufferSize)
+        {
+            const string SettingName = "MessageBuffer.BufferSize";
+
+            configurable.AddSetting(SettingName, bufferSize);
+            configurable.Configure(context =>
+            {
+                var serializer = (ISerializer<T>)context.GetService(typeof(ISerializer<T>));
+                var stream = (IStream)context.GetService(typeof(IStream));
+                var buffSize = (int)context.GetSetting(SettingName);
+                var messageBuffer = new MessageBuffer<T>(serializer.Serialize, new byte[bufferSize], stream.WriteAsync);
+            });
+        }
+
+        public static void UseMessageReceiver<T>(this IConfigurable configurable)
+        {
+            configurable.Configure(context =>
+            {
+                var reader = (IBufferReader)context.GetService(typeof(IBufferReader));
+                var serializer = (ISerializer<T>)context.GetService(typeof(ISerializer<T>));
+                var receiver = new MessageReceiver<T>(serializer.Deserialize, reader);
+
+                context.RegisterService(typeof(IReceiver<T>), receiver);
+            });
+        }
+
+        public static void UseBufferReader(this IConfigurable configurable, int bufferSize)
+        {
+            const string SettingName = "BufferReader.BufferSize";
+
+            configurable.AddSetting(SettingName, bufferSize);
+            configurable.Configure(context =>
+            {
+                var buffSize = (int)context.GetSetting(SettingName);
+                var stream = (IStream)context.GetService(typeof(IStream));
+                var reader = new BufferReader(new byte[buffSize], stream.ReadAsync);
+
+                context.RegisterService(typeof(IBufferReader), reader);
+            });
+        }
     }
 }
