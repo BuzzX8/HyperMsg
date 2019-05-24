1357669146052112d98c986d36f8a8a841560bf2
Max Gunther <maxim.chasnik@gmail.com>
2019-05-24 15:39:25 +0300

    ConfigurableBuilder bugfix (#49)

--- test/HyperMsg.Core.Tests/ConfigurableBuilderTests.cs
+++ test/HyperMsg.Core.Tests/ConfigurableBuilderTests.cs
@@ -5,17 +5,11 @@ using Xunit;
 namespace HyperMsg
 {
     public class ConfigurableBuilderTests
-    {        
-        private readonly ConfigurableBuilder<string> builder;
-
-        public ConfigurableBuilderTests()
-        {
-            builder = new ConfigurableBuilder<string>();
-        }
-
+    {
         [Fact]
-        public void Build_Invokes_Configurators()
+        public void Build_Invokes_All_Configurators()
         {
+            var builder = new ConfigurableBuilder<string>();
             var configurators = A.CollectionOfFake<Action<IConfigurationContext>>(10);
             builder.Configure(c => c.RegisterService(typeof(string), ""));
 
@@ -35,6 +29,7 @@ namespace HyperMsg
         [Fact]
         public void Build_Returns_Registered_Service()
         {
+            var builder = new ConfigurableBuilder<string>();
             var expected = Guid.NewGuid().ToString();
             builder.Configure(c =>
             {
@@ -49,6 +44,7 @@ namespace HyperMsg
         [Fact]
         public void ConfigurationContext_GetSetting_Returns_Previuosly_Added_Setting()
         {
+            var builder = new ConfigurableBuilder<string>();
             var expected = Guid.NewGuid();
             var actual = Guid.Empty;
             builder.AddSetting(nameof(Guid), expected);
@@ -64,40 +60,81 @@ namespace HyperMsg
         }
 
         [Fact]
-        public void ConfigurationContext_GetService_Returns_Previously_Registered_Service()
+        public void Build_Resolves_Direct_Dependency_Registered_Before_Service()
         {
+            var builder = new ConfigurableBuilder<IBufferReader>();
             var expected = Guid.NewGuid();
             var actual = Guid.Empty;
-            builder.Configure(c => c.RegisterService(typeof(Guid), expected));
 
-            builder.Configure(c =>
+            builder.Configure(c => c.RegisterService(typeof(IStream), A.Fake<IStream>()));
+            builder.UseBufferReader(100);
+
+            var reader = builder.Build();
+
+            Assert.NotNull(reader);
+        }
+
+        [Fact]
+        public void Build_Resolves_Direct_Dependency_Registered_After_Service()
+        {
+            var builder = new ConfigurableBuilder<IBufferReader>();
+            var expected = Guid.NewGuid();
+            var actual = Guid.Empty;
+
+            builder.UseBufferReader(100);
+            builder.Configure(c => c.RegisterService(typeof(IStream), A.Fake<IStream>()));            
+
+            var reader = builder.Build();
+
+            Assert.NotNull(reader);
+        }
+
+        [Fact]
+        public void Build_Resolves_Complex_Dependencies()
+        {
+            var builder = new ConfigurableBuilder<string>();
+            var expected = Guid.NewGuid().ToString();
+            builder.UseCoreServices<Guid>(100, 100);
+            builder.Configure(context =>
             {
-                actual = (Guid)c.GetService(typeof(Guid));
-                c.RegisterService(typeof(string), "");
+                context.RegisterService(typeof(ISerializer<Guid>), A.Fake<ISerializer<Guid>>());
+                context.RegisterService(typeof(IStream), A.Fake<IStream>());
+                context.RegisterService(typeof(IHandler<TransportCommands>), A.Fake<IHandler<TransportCommands>>());
+            });
+            builder.Configure(context =>
+            {
+                var transceiver = (ITransceiver<Guid, Guid>)context.GetService(typeof(ITransceiver<Guid, Guid>));
+                context.RegisterService(typeof(string), expected);
             });
 
-            builder.Build();
+            var actual = builder.Build();
 
             Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void ConfigurationContext_GetService_Returns_Service_Registered_After()
+        public void Build_Throws_Exception_If_No_Registered_Dependencies()
         {
-            var expected = Guid.NewGuid();
-            var actual = Guid.Empty;
-
-            builder.Configure(c =>
+            var builder = new ConfigurableBuilder<string>();
+            var expected = Guid.NewGuid().ToString();
+            builder.UseCoreServices<Guid>(100, 100);
+            builder.Configure(context =>
             {
-                actual = (Guid)c.GetService(typeof(Guid));
-                c.RegisterService(typeof(string), "");
+                var transceiver = (ITransceiver<Guid, Guid>)context.GetService(typeof(ITransceiver<Guid, Guid>));
+                context.RegisterService(typeof(string), expected);
             });
 
-            builder.Configure(c => c.RegisterService(typeof(Guid), expected));            
+            Assert.Throws<InvalidOperationException>(() => builder.Build());
+        }
 
-            builder.Build();
+        [Fact]
+        public void Builder_Rethrows_Exception_Thrown_By_Configurator()
+        {
+            var builder = new ConfigurableBuilder<string>();
+            builder.Configure(context => throw new ArgumentNullException());
+            builder.Configure(context => context.RegisterService(typeof(string), ""));
 
-            Assert.Equal(expected, actual);
+            Assert.Throws<AggregateException>(() => builder.Build());
         }
     }
 }
