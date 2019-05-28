eac6899058e900ccc454ffaf5f70a46b6705790b
Max Gunther <maxim.chasnik@gmail.com>
2019-05-28 16:48:44 +0300

    Integration tests (#48)

---
+++ test/HyperMsg.Integration.Tests/IJsonClient.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg.Integration
+{
+    public interface IJsonClient
+    {
+        void Connect();
+
+        Task ConnectAsync(CancellationToken cancellationToken);
+
+        void Disconnect();
+
+        Task DisconnectAsync(CancellationToken cancellationToken);
+
+        void SendObject(JObject @object);
+
+        Task SendObjectAsync(JObject @object, CancellationToken cancellationToken);
+
+        void SendObjects(IEnumerable<JObject> objects);
+
+        Task SendObjectsAsync(IEnumerable<JObject> objects, CancellationToken cancellationToken);
+
+        event EventHandler<ObjectReceivedEventArgs> ObjectReceived;
+    }
+
+    public class ObjectReceivedEventArgs : EventArgs
+    {
+        public ObjectReceivedEventArgs(JObject @object)
+        {
+            Object = @object;
+        }
+
+        public JObject Object { get; }
+    }
+}
---
+++ test/HyperMsg.Integration.Tests/JsonClient.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace HyperMsg.Integration
+{
+    public class JsonClient : IJsonClient, IHandler<JObject>
+    {
+        private readonly IMessageBuffer<JObject> messageBuffer;
+        private readonly IHandler<TransportCommands> transportHandler;
+        private readonly IHandler<ReceiveMode> receiveModeHandler;
+
+        public JsonClient(IMessageBuffer<JObject> messageBuffer, IHandler<TransportCommands> transportHandler, IHandler<ReceiveMode> receiveModeHandler)
+        {
+            this.messageBuffer = messageBuffer;
+            this.transportHandler = transportHandler;
+            this.receiveModeHandler = receiveModeHandler;
+        }
+
+        public void Connect()
+        {
+            transportHandler.Handle(TransportCommands.OpenConnection);
+            receiveModeHandler.Handle(ReceiveMode.Reactive);
+        }
+
+        public async Task ConnectAsync(CancellationToken cancellationToken)
+        {
+            await transportHandler.HandleAsync(TransportCommands.OpenConnection, cancellationToken);
+            await receiveModeHandler.HandleAsync(ReceiveMode.Proactive, cancellationToken);
+        }
+
+        public void Disconnect() => transportHandler.Handle(TransportCommands.CloseConnection);
+
+        public Task DisconnectAsync(CancellationToken cancellationToken) => transportHandler.HandleAsync(TransportCommands.CloseConnection, cancellationToken);
+
+        public void SendObject(JObject @object)
+        {
+            messageBuffer.Write(@object);
+            messageBuffer.Flush();
+        }
+
+        public Task SendObjectAsync(JObject @object, CancellationToken cancellationToken)
+        {
+            messageBuffer.Write(@object);
+            return messageBuffer.FlushAsync(cancellationToken);
+        }
+
+        public void SendObjects(IEnumerable<JObject> objects)
+        {
+            foreach (var @object in objects)
+            {
+                messageBuffer.Write(@object);
+            }
+
+            messageBuffer.Flush();
+        }
+
+        public Task SendObjectsAsync(IEnumerable<JObject> objects, CancellationToken cancellationToken)
+        {
+            foreach (var @object in objects)
+            {
+                messageBuffer.Write(@object);
+            }
+
+            return messageBuffer.FlushAsync(cancellationToken);
+        }
+
+        public void Handle(JObject message)
+        {
+            OnObjectReceived(message);
+        }
+
+        public Task HandleAsync(JObject message, CancellationToken cancellationToken = default)
+        {
+            OnObjectReceived(message);
+            return Task.CompletedTask;
+        }
+
+        private void OnObjectReceived(JObject @object)
+        {
+            ObjectReceived?.Invoke(this, new ObjectReceivedEventArgs(@object));
+        }
+
+        public event EventHandler<ObjectReceivedEventArgs> ObjectReceived;
+    }
+}
