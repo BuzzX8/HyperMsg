a2caa5323387ae45ae44ce1f15aa8ea18b0b5384
Max Gunther <maxim.chasnik@gmail.com>
2019-05-28 16:22:29 +0300

    IConfigurable redesign (#50)

--- src/HyperMsg.Sockets/ConfigurableExtensions.cs
+++ src/HyperMsg.Sockets/ConfigurableExtensions.cs
@@ -7,15 +7,11 @@ namespace HyperMsg.Sockets
         public static void UseSockets(this IConfigurable configurable, EndPoint endpoint)
         {
             configurable.AddSetting(nameof(EndPoint), endpoint);
-            configurable.Configure(AddSocketServices);
-        }
-
-        private static void AddSocketServices(IConfigurationContext context)
-        {
-            var socket = new SocketProxy(SocketFactory.CreateTcpSocket(), (EndPoint)context.GetSetting(nameof(EndPoint)));
-            var transport = new SocketTransport(socket);
-            context.RegisterService(typeof(IStream), transport);
-            context.RegisterService(typeof(IHandler<TransportCommands>), transport);
+            configurable.AddService(new[] { typeof(IStream), typeof(IHandler<TransportCommands>) }, (p, s) =>
+            {
+                var socket = new SocketProxy(SocketFactory.CreateTcpSocket(), (EndPoint)s[nameof(EndPoint)]);
+                return new SocketTransport(socket);
+            });
         }
     }
 }
