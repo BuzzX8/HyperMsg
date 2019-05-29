53525b6e1824546782c6d21e3f9455a0abf1370a
BuzzX8 <maksym.chasnik@gmail.com>
2019-05-29 09:34:19 +0300

    Added DelegateHandler

---
+++ test/HyperMsg.Core.Tests/DelegateHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HyperMsg
+{
+    public class DelegateHandlerTests
+    {
+        [Fact]
+        public void Handle_Calls_HandleAction()
+        {
+            var expected = Guid.NewGuid();
+            var actual = Guid.Empty;
+            var handler = new DelegateHandler<Guid>(m => actual = m);
+
+            handler.Handle(expected);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task HandleAsync_Calls_HandleAction_If_HandleAsyncFunction_Not_Provided()
+        {
+            var expected = Guid.NewGuid();
+            var actual = Guid.Empty;
+            var handler = new DelegateHandler<Guid>(m => actual = m);
+
+            await handler.HandleAsync(expected);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task HandleAsync_Calls_HandleAsyncFunction_If_Provided()
+        {
+            var expected = Guid.NewGuid();
+            var actual = Guid.Empty;
+            var handler = new DelegateHandler<Guid>(_ => { }, (m, t) =>
+            {
+                actual = m;
+                return Task.FromResult(expected);
+            });
+
+            await handler.HandleAsync(expected);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
