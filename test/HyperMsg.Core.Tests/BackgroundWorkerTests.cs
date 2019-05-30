a316fd937f12133977102f96f80024a742a8ce04
BuzzX8 <maksym.chasnik@gmail.com>
2019-05-30 11:03:27 +0300

    wip

--- test/HyperMsg.Core.Tests/BackgroundWorkerTests.cs
+++ test/HyperMsg.Core.Tests/BackgroundWorkerTests.cs
@@ -5,14 +5,17 @@ using Xunit;
 
 namespace HyperMsg
 {
-    public class BackgroundWorkerTests
+    public class BackgroundWorkerTests : IDisposable
     {
+        private BackgroundWorkerImpl worker;
+        private readonly TimeSpan waitTimeOut = TimeSpan.FromSeconds(2);
+
         [Fact]
         public void Run_Periodically_Invokes_DoWorkIterationAsync()
         {
             var invokeCount = 5;
             var @event = new ManualResetEventSlim();
-            var worker = new BackgroundWorkerImpl(t => 
+            worker = new BackgroundWorkerImpl(t => 
             {
                 if (invokeCount == 0)
                 {
@@ -25,18 +28,18 @@ namespace HyperMsg
             });
 
             worker.InvokeRun();
-            @event.Wait(TimeSpan.FromSeconds(2));
+            @event.Wait(waitTimeOut);
 
             Assert.Equal(0, invokeCount);
         }
 
         [Fact]
-        public void Stop_Stops_Invoking_DoWorkIterationAsync()
+        public void Stop_Stops_Invoking_DoWorkIterationAsync_And_Invokes_BackgroundTaskCompleted()
         {
             var @event = new ManualResetEventSlim();
             var @event2 = new ManualResetEventSlim();
             var wasInvoked = false;
-            var worker = new BackgroundWorkerImpl(t =>
+            worker = new BackgroundWorkerImpl(t =>
             {
                 @event.Wait();
                 wasInvoked = true;
@@ -47,10 +50,50 @@ namespace HyperMsg
 
             worker.InvokeStop();
             @event.Set();
-            event2.Wait(TimeSpan.FromSeconds(2));
+            event2.Wait(waitTimeOut);
 
             Assert.False(wasInvoked);
         }
+
+        [Fact]
+        public void Invokes_UnhandledException_When_DoWorkIterationAsync_Throws_Exception()
+        {
+            var @event = new ManualResetEventSlim();
+            var expected = new InvalidOperationException();
+            var actual = default(Exception);
+            worker = new BackgroundWorkerImpl(t => throw expected);
+            worker.UnhandledException += e =>
+            {
+                actual = e;
+                @event.Set();
+            };
+
+            worker.InvokeRun();
+            @event.Wait(waitTimeOut);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Dispose_Initiates_Background_Task_Cancellation()
+        {
+            var @event = new ManualResetEventSlim();
+            var isTaskCompleted = false;
+            worker = new BackgroundWorkerImpl(t => Task.Delay(waitTimeOut));
+            worker.BackgroundTaskCompleted += t =>
+            {
+                isTaskCompleted = true;
+                @event.Set();
+            };
+            worker.InvokeRun();
+
+            worker.Dispose();
+            @event.Wait(waitTimeOut);
+
+            Assert.True(isTaskCompleted);
+        }
+
+        public void Dispose() => worker?.Dispose();
     }
 
     public class BackgroundWorkerImpl : BackgroundWorker
