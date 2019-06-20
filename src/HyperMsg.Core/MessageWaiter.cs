998d17d3848e37526c5eb1f0c8c9c7b0c129eff0
BuzzX8 <maksym.chasnik@gmail.com>
2019-06-20 17:33:31 +0300

    Added MessageWaiter

---
+++ src/HyperMsg.Core/MessageWaiter.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg
+{
+    public class MessageWaiter<T>
+    {        
+        private TaskCompletionSource<T> completionSource;
+        private object message;
+
+        public Task<T> WaitAsync(CancellationToken cancellationToken)
+        {
+            if (message != null)
+            {
+                return Task.FromResult((T)message);
+            }
+
+            return (completionSource = new TaskCompletionSource<T>()).Task;            
+        }
+
+        public void SetMessage(T message)
+        {
+            if(completionSource == null)
+            {
+                this.message = message;
+                return;
+            }
+
+            completionSource.SetResult(message);
+            completionSource = null;
+        }
+    }
+}
---
+++ test/HyperMsg.Core.Tests/MessageWaiterTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+
+namespace HyperMsg
+{
+    public class MessageWaiterTests
+    {
+        [Fact]
+        public void WaitAsync_Returns_Completed_Task_If_Message_Has_Been_Set()
+        {
+            var message = Guid.NewGuid();
+            var waiter = new MessageWaiter<Guid>();
+
+            waiter.SetMessage(message);
+            var task = waiter.WaitAsync(CancellationToken.None);
+
+            Assert.True(task.IsCompletedSuccessfully);
+            Assert.Equal(message, task.Result);
+        }
+
+        [Fact]
+        public void SetMessage_Completes_Waiting_Task()
+        {
+            var message = Guid.NewGuid();
+            var waiter = new MessageWaiter<Guid>();
+
+            var task = waiter.WaitAsync(CancellationToken.None);
+            waiter.SetMessage(message);
+
+            Assert.True(task.IsCompletedSuccessfully);
+            Assert.Equal(message, task.Result);
+        }
+    }
+}
