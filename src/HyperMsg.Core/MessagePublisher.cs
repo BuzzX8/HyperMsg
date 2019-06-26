86df2b0fd13e9f9e391d33c36eef6c0b0d1adfc6
BuzzX8 <maksym.chasnik@gmail.com>
2019-06-26 10:09:22 +0300

    Restored MessagePublisher

---
+++ src/HyperMsg.Core/MessagePublisher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg
+{
+    public class MessagePublisher : IHandlerRegistry
+    {
+        private readonly Dictionary<Type, List<object>> handlers = new Dictionary<Type, List<object>>();
+
+        public void Register<T>(IMessageHandler<T> handler)
+        {
+            if (!handlers.ContainsKey(typeof(T)))
+            {
+                handlers.Add(typeof(T), new List<object>());
+            }
+
+            handlers[typeof(T)].Add(handler);
+        }
+
+        public async Task PublishAsync<T>(T message, CancellationToken cancellationToken)
+        {
+            if (!handlers.ContainsKey(typeof(T)))
+            {
+                return;
+            }
+
+            foreach (var handler in handlers[typeof(T)].Cast<IMessageHandler<T>>())
+            {
+                await handler.HandleAsync(message, cancellationToken);
+            }
+        }
+    }
+}
---
+++ test/HyperMsg.Core.Tests/MessagePublisherTests.cs
@@ -0,0 +1,38 @@
+﻿using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HyperMsg
+{
+    public class MessagePublisherTests
+    {
+        private readonly MessagePublisher publisher = new MessagePublisher();
+
+        [Fact]
+        public async Task SendAsync_Calls_Handle_For_Each_Registered_Handler()
+        {
+            var handlers = A.CollectionOfFake<IMessageHandler<string>>(4);
+            AddHandlers(handlers);
+            var expected = Guid.NewGuid().ToString();
+            var cancellationToken = new CancellationToken();
+
+            await publisher.PublishAsync(expected, cancellationToken);
+
+            foreach (var handler in handlers)
+            {
+                A.CallTo(() => handler.HandleAsync(expected, cancellationToken)).MustHaveHappened();
+            }
+        }
+
+        private void AddHandlers<T>(IEnumerable<IMessageHandler<T>> handlers)
+        {
+            foreach (var handler in handlers)
+            {
+                publisher.Register(handler);
+            }
+        }
+    }
+}
