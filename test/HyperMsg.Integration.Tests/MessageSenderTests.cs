cfa0d12d653b2be8dbab89ff1dec659c1e61cd43
BuzzX8 <maksym.chasnik@gmail.com>
2019-07-03 09:47:18 +0300

    Refactored tests, added test for MessageSender

---
+++ test/HyperMsg.Integration.Tests/MessageSenderTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HyperMsg.Integration
+{
+    public class MessageSenderTests : TestFixtureBase
+    {
+        private readonly IMessageSender<Guid> messageSender;
+
+        public MessageSenderTests()
+        {
+            messageSender = ServiceProvider.GetService<IMessageSender<Guid>>();
+        }
+
+        [Fact]
+        public async Task SendAsync_Transmits_Message_Over_Transport()
+        {
+            var expectedMessage = Guid.NewGuid();
+            var actualMessage = Guid.Empty;
+            await OpenTransportAsync();
+
+            await messageSender.SendAsync(expectedMessage, CancellationToken.None);
+            var receiveBuffer = new byte[100];
+            var received = AcceptedSocket.Receive(receiveBuffer);
+            actualMessage = new Guid(new ReadOnlySpan<byte>(receiveBuffer, 0, received));
+
+            Assert.Equal(expectedMessage, actualMessage);
+        }
+    }
+}
---
+++ test/HyperMsg.Integration.Tests/TestFixtureBase.cs
@@ -0,0 +1,47 @@
+﻿using HyperMsg.Sockets;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg.Integration
+{
+    public abstract class TestFixtureBase : IDisposable
+    {
+        protected readonly IPEndPoint EndPoint = new IPEndPoint(IPAddress.Loopback, 8080);
+        protected readonly ConfigurableServiceProvider ServiceProvider;
+
+        protected readonly ITransport Transport;
+        protected Socket AcceptedSocket;
+
+        private Socket listeningSocket;
+
+        protected TestFixtureBase()
+        {
+            ServiceProvider = new ConfigurableServiceProvider();
+            ServiceProvider.UseCoreServices<Guid>(2048, 2048);
+            ServiceProvider.UseSockets(EndPoint);
+            ServiceProvider.UseGuidSerializer();
+
+            Transport = ServiceProvider.GetService<ITransport>();
+
+            listeningSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            listeningSocket.Bind(EndPoint);
+            listeningSocket.Listen(1);
+        }
+
+        protected async Task OpenTransportAsync()
+        {
+            var acceptTask = Task.Run(() => listeningSocket.Accept());
+            await Transport.ProcessCommandAsync(TransportCommand.Open, CancellationToken.None);
+            AcceptedSocket = await acceptTask;
+        }
+
+        public void Dispose()
+        {
+            AcceptedSocket?.Close();
+            listeningSocket.Close();
+        }
+    }
+}
