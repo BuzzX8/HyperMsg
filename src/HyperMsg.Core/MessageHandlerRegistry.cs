62bdc817d7a2cc984e6a818ee829cddf3dc1fc4a
Max Gunther <maxim.chasnik@gmail.com>
2019-07-03 09:09:10 +0300

    Refactored IMessageHandlerRegistry (#66)

---
+++ src/HyperMsg.Core/MessageHandlerRegistry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HyperMsg
+{
+    public class MessageHandlerRegistry<T> : IMessageHandlerRegistry<T>
+    {
+        private Action<T> handlers;
+
+        public void Register(Action<T> handler) => handlers += handler;
+
+        public void Handle(T message)
+        {
+            handlers?.Invoke(message);
+        }
+    }
+}
---
+++ test/HyperMsg.Core.Tests/MessageHandlerRegistryTests.cs
@@ -0,0 +1,35 @@
+﻿using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace HyperMsg
+{
+    public class MessageHandlerRegistryTests
+    {
+        private readonly MessageHandlerRegistry<Guid> publisher = new MessageHandlerRegistry<Guid>();
+
+        [Fact]
+        public void Handle_Calls_Registered_Handlers()
+        {
+            var handlers = A.CollectionOfFake<Action<Guid>>(4);
+            AddHandlers(handlers);
+            var expected = Guid.NewGuid();
+
+            publisher.Handle(expected);
+
+            foreach (var handler in handlers)
+            {
+                A.CallTo(() => handler.Invoke(expected)).MustHaveHappened();
+            }
+        }
+
+        private void AddHandlers(IEnumerable<Action<Guid>> handlers)
+        {
+            foreach (var handler in handlers)
+            {
+                publisher.Register(handler);
+            }
+        }
+    }
+}
