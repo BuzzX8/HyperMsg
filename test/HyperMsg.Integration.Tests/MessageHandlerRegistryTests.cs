f57be9657c68a0dd67b1dbac29c54ee9367a8f2c
BuzzX8 <maksym.chasnik@gmail.com>
2019-07-03 10:50:57 +0300

    wip

--- test/HyperMsg.Integration.Tests/MessageHandlerRegistryTests.cs
+++ test/HyperMsg.Integration.Tests/MessageHandlerRegistryTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -19,20 +21,27 @@ namespace HyperMsg.Integration
         [Fact]
         public async Task Registered_Handler_Invoked_When_Message_Received()
         {
-            var expectedMessage = Guid.NewGuid();
-            var actualMessage = Guid.Empty;
+            var expectedMessages = Enumerable.Range(0, 10).Select(i => Guid.NewGuid()).ToList();
+            var actualMessages = new List<Guid>();
             var @event = new ManualResetEventSlim();
+
+            var received = 0;
             registry.Register(g =>
             {
-                actualMessage = g;
-                @event.Set();
+                actualMessages.Add(g);
+                received++;
+
+                if (received == expectedMessages.Count)
+                {
+                    @event.Set();
+                }
             });
 
             await OpenTransportAsync();
-            var sent = AcceptedSocket.Send(expectedMessage.ToByteArray());
+            expectedMessages.ForEach(g => AcceptedSocket.Send(g.ToByteArray()));
             @event.Wait(waitTimeout);
 
-            Assert.Equal(expectedMessage, actualMessage);
+            Assert.Equal(expectedMessages, actualMessages);
         }
     }
 }
