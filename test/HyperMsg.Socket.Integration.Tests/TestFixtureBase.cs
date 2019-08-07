baa1824a60645ea35e4025051c31cbb20f434c3a
Max Gunther <maxim.chasnik@gmail.com>
2019-08-07 20:23:46 +0300

    Integration fixture update (#70)

--- test/HyperMsg.Socket.Integration.Tests/TestFixtureBase.cs
+++ test/HyperMsg.Socket.Integration.Tests/TestFixtureBase.cs
@@ -1,39 +1,31 @@
-﻿using HyperMsg.Sockets;
-using System;
-using System.Net;
+﻿using System;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace HyperMsg.Integration
 {
-    public abstract class SocketFixtureBase<T> : IntegrationFixtureBase<T>, IDisposable
+    public abstract class TestFixtureBase : SocketTransportFixtureBase<Guid>, IDisposable
     {
-        private readonly IPEndPoint endPoint;
+
         private readonly Socket listeningSocket;
         private readonly byte[] receiveBuffer;
-
         protected Socket AcceptedSocket;
 
-        protected SocketFixtureBase(int port) : this(IPAddress.Loopback, port)
-        { }
-
-        protected SocketFixtureBase(IPAddress address, int port)
+        protected TestFixtureBase(int port) : base(port)
         {
-            endPoint = new IPEndPoint(address, port);
-
             listeningSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-            listeningSocket.Bind(endPoint);
+            listeningSocket.Bind(EndPoint);
             listeningSocket.Listen(1);
 
             receiveBuffer = new byte[200];
         }
 
-        protected override void ConfigureTransport(IConfigurable configurable) => configurable.UseSockets(endPoint);
+        protected override void ConfigureSerializer(IConfigurable configurable) => configurable.UseGuidSerializer();
 
         protected override async Task OpenTransportAsync(CancellationToken cancellationToken = default)
         {
-            var acceptTask = Task.Run(() => listeningSocket.Accept());
+            var acceptTask = Task.Run(() => listeningSocket.Accept(), cancellationToken);
             await base.OpenTransportAsync(cancellationToken);
             AcceptedSocket = await acceptTask;
         }
