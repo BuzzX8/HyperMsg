c074625ae1ef172cf53fbde5e2b667593bc95670
BuzzX8 <maxim.chasnik@gmail.com>
2019-08-07 20:51:44 +0300

    wip

---
+++ src/HyperMsg/IBuffer.cs
@@ -0,0 +1,17 @@
+﻿using System.Buffers;
+
+namespace HyperMsg
+{
+    public interface IBuffer
+    {
+        IBufferWriter<byte> Writer { get; }
+
+        event AsyncAction<IBufferReader> DataCommitted;
+    }
+
+    public interface ISendingBuffer : IBuffer
+    { }
+
+    public interface IReceivingBuffer : IBuffer
+    { }
+}
