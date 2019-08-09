c28311e8cf8e598319947fe299605b98fabfa6b8
BuzzX8 <maksym.chasnik@gmail.com>
2019-08-09 16:19:35 +0300

    wip

--- src/HyperMsg.Core/Buffer.cs
+++ src/HyperMsg.Core/Buffer.cs
@@ -3,12 +3,21 @@ using System.Buffers;
 
 namespace HyperMsg
 {
-    public class Buffer : IBuffer
+    public class Buffer : IBuffer, IDisposable
     {
+        private readonly IMemoryOwner<byte> memoryOwner;
+
+        public Buffer(IMemoryOwner<byte> memoryOwner)
+        {
+            this.memoryOwner = memoryOwner ?? throw new ArgumentNullException(nameof(memoryOwner));
+        }
+
         public IBufferReader<byte> Reader => throw new NotImplementedException();
 
         public IBufferWriter<byte> Writer => throw new NotImplementedException();
 
         public event AsyncAction<IBuffer> Flushed;
+
+        public void Dispose() => memoryOwner.Dispose();
     }
 }
--- test/HyperMsg.Core.Tests/BufferTests.cs
+++ test/HyperMsg.Core.Tests/BufferTests.cs
@@ -1,9 +1,51 @@
-﻿using System;
+﻿using FakeItEasy;
+using System;
+using System.Buffers;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace HyperMsg
 {
     public class BufferTests
     {
+        private readonly IMemoryOwner<byte> memoryOwner;
+        private readonly Memory<byte> memory;
         private readonly Buffer buffer;
+
+        const int MemorySize = 4096;
+
+        public BufferTests()
+        {
+            memoryOwner = A.Fake<IMemoryOwner<byte>>();
+            memory = new byte[MemorySize];
+            A.CallTo(() => memoryOwner.Memory).Returns(memory);
+            buffer = new Buffer(memoryOwner);
+        }
+
+        [Fact]
+        public async Task ReadAsync_Returns_Written_Bytes()
+        {
+            var expectedBytes = Guid.NewGuid().ToByteArray();
+            WriteBytes(expectedBytes);
+
+            var actualBytes = await buffer.Reader.ReadAsync(default);
+
+            Assert.Equal(expectedBytes, actualBytes.ToArray());
+        }
+
+        [Fact]
+        public void Dispose_Disposes_MemoryOwner()
+        {
+            buffer.Dispose();
+
+            A.CallTo(() => memoryOwner.Dispose()).MustHaveHappened();
+        }
+
+        private void WriteBytes(byte[] bytes)
+        {
+            var memory = buffer.Writer.GetMemory(bytes.Length);
+            bytes.CopyTo(memory);
+            buffer.Writer.Advance(bytes.Length);
+        }
     }
 }
