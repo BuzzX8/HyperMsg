e40450287fb4e8dd89716b31193b10a38e142a9d
BuzzX8 <maksym.chasnik@gmail.com>
2019-11-14 11:24:07 +0200

    wip

---
+++ src/HyperMsg/MessageSenderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg
+{
+    public static class MessageSenderExtensions
+    {
+        public static void Received<T>(this IMessageSender messageSender, T message) => messageSender.Send(new Received<T>(message));
+
+        public static Task ReceivedAsync<T>(this IMessageSender messageSender, T message, CancellationToken cancellationToken) => messageSender.SendAsync(new Received<T>(message), cancellationToken);
+
+        public static void Transmit<T>(this IMessageSender messageSender, T message) => messageSender.Send(new Transmit<T>(message));
+
+        public static Task TransmitAsync<T>(this IMessageSender messageSender, T message, CancellationToken cancellationToken) => messageSender.SendAsync(new Transmit<T>(message), cancellationToken);
+    }
+}
