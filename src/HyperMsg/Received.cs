229cd16e4154d3eabe893077184c433ef38cc8c7
BuzzX8 <maksym.chasnik@gmail.com>
2020-01-17 16:47:57 +0200

    wip

--- src/HyperMsg/Received.cs
+++ src/HyperMsg/Received.cs
@@ -1,10 +1,12 @@
-﻿namespace HyperMsg
+﻿using System;
+
+namespace HyperMsg
 {
     /// <summary>
     /// Decorator that represents message received from transport layer.
     /// </summary>
     /// <typeparam name="T">Message type.</typeparam>
-    public struct Received<T>
+    public struct Received<T> : IEquatable<Received<T>>
     {
         public Received(T message) => Message = message;
 
@@ -14,5 +16,25 @@
         public T Message { get; }
 
         public static implicit operator T(Received<T> received) => received.Message;
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != typeof(Received<T>))
+            {
+                return false;
+            }
+
+            return Equals((Received<T>)obj);
+        }
+
+        public bool Equals(Received<T> other) => Message.Equals(other);
+
+        public override int GetHashCode() => Message.GetHashCode();
+
+        public override string ToString() => $"Received({Message})";
+
+        public static bool operator ==(Received<T> left, Received<T> right) => left.Equals(right);
+
+        public static bool operator !=(Received<T> left, Received<T> right) => !(left == right);
     }
 }
---
+++ test/HyperMsg.Extensions.Tests/MessageSenderExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using FakeItEasy;
+using System;
+using Xunit;
+
+namespace HyperMsg
+{
+    public class MessageSenderExtensionsTests
+    {
+        private readonly IMessageSender messageSender = A.Fake<IMessageSender>();
+        private readonly string message = Guid.NewGuid().ToString();
+
+        [Fact]
+        public void Received_Sends_Message_Decorated_With_Received()
+        {
+            messageSender.Received(message);
+
+            A.CallTo(() => messageSender.Send<string>(new Received<string>(message))).MustHaveHappened();
+        }
+    }
+}
