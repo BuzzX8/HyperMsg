589711bc1902580e077e636f1a75718315e9f2b5
BuzzX8 <maksym.chasnik@gmail.com>
2020-01-20 16:25:16 +0200

    wip

---
+++ test/HyperMsg.Extensions.Tests/ServiceProviderExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HyperMsg
+{
+    public class ServiceProviderExtensionsTests
+    {
+        private readonly IServiceProvider serviceProvider = A.Fake<IServiceProvider>();
+
+        [Fact]
+        public void GetService_Returns_Service_Of_Specified_Type()
+        {
+            var service = A.Fake<IMessageSender>();
+            A.CallTo(() => serviceProvider.GetService(typeof(IMessageSender))).Returns(service);
+
+            var actual = serviceProvider.GetService<IMessageSender>();
+
+            Assert.Same(service, actual);
+        }
+
+        [Fact]
+        public void GetService_Throws_Exception_If_Incorrect_Service_Type_Provided()
+        {
+            var service = A.Fake<IMessageHandlerRegistry>();
+            A.CallTo(() => serviceProvider.GetService(typeof(IMessageSender))).Returns(service);
+
+            Assert.Throws<InvalidOperationException>(() => serviceProvider.GetService<IMessageSender>());
+        }
+
+        [Fact]
+        public void GetService_Throws_Exception_()
+        {
+            A.CallTo(() => serviceProvider.GetService(A<Type>._)).Returns(null);
+
+            Assert.Throws<InvalidOperationException>(() => serviceProvider.GetService<IMessageSender>(true));
+        }
+    }
+}
