b97faef61708f74b38e3ca063bc7897ea8d00a42
BuzzX8 <maksym.chasnik@gmail.com>
2020-01-20 14:31:22 +0200

    wip

---
+++ src/HyperMsg.Extensions/ConfigurationSettingsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HyperMsg
+{
+    public static class ConfigurationSettingsExtensions
+    {
+        public static T Get<T>(this IConfigurationSettings settings, string key)
+        {
+            var value = settings[key];
+
+            if (value.GetType() != typeof(T))
+            {
+                throw new InvalidOperationException();
+            }
+
+            return (T)value;
+        }        
+    }
+}
---
+++ src/HyperMsg/IConfigurationSettings.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace HyperMsg
+{
+    public interface IConfigurationSettings : IReadOnlyDictionary<string, object>
+    { }
+}
