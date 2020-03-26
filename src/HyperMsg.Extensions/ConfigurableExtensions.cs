3822d5ecc0cf792a22d098a64a18b8249aa5ac83
Max Gunther <maxim.chasnik@gmail.com>
2020-03-26 12:13:39 +0200

    New Configurable extensions (#87)

--- src/HyperMsg.Extensions/ConfigurableExtensions.cs
+++ src/HyperMsg.Extensions/ConfigurableExtensions.cs
@@ -1,7 +1,14 @@
-﻿namespace HyperMsg
+﻿using System;
+
+namespace HyperMsg
 {
     public static class ConfigurableExtensions
     {
         public static void AddService<T>(this IConfigurable configurable, T serviceInstance) => configurable.AddService(typeof(T), serviceInstance);
+
+        public static void AddService<T>(this IConfigurable configurable, Func<IServiceProvider, T> serviceFactory)
+        {
+            configurable.AddService(typeof(T), provider => serviceFactory(provider));
+        }
     }
 }
