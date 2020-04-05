057cfb3b75c3afd288c7cfccda2d920d2908c4f1
BuzzX8 <maxim.chasnik@gmail.com>
2020-04-05 16:55:21 +0300

    wip

--- src/HyperMsg/IMessageObservable.cs
+++ src/HyperMsg/IMessageObservable.cs
@@ -11,14 +11,14 @@ namespace HyperMsg
         /// Registers synchronous message handler.
         /// </summary>
         /// <typeparam name="T">Type of message.</typeparam>
-        /// <param name="observer">Message observer.</param>
-        void Subscribe<T>(Action<T> observer);
+        /// <param name="messageObserver">Message observer.</param>
+        void Subscribe<T>(Action<T> messageObserver);
 
         /// <summary>
         /// Registers asynchronous message handler.
         /// </summary>
         /// <typeparam name="T">Typeof message.</typeparam>
-        /// <param name="observer">Message observer.</param>
-        void Subscribe<T>(AsyncAction<T> observer);
+        /// <param name="messageObserver">Message observer.</param>
+        void Subscribe<T>(AsyncAction<T> messageObserver);
     }
 }
