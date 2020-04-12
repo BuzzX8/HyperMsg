dd850026888a903ed7e7e9e4d2313012dd68f144
Max Gunther <maxim.chasnik@gmail.com>
2020-04-12 14:22:41 +0300

    Extensions for MessageObservable (#91)

---
+++ src/HyperMsg.Extensions/MessageObservableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HyperMsg
+{
+    public static class MessageObservableExtensions
+    {
+        public static void SubscribeTransmitter<T>(this IMessageObservable messageObservable, Action<T> messageObserver)
+        {
+            messageObservable.Subscribe<Transmit<T>>(m => messageObserver.Invoke(m));
+        }
+
+        public static void SubscribeTransmitter<T>(this IMessageObservable messageObservable, AsyncAction<T> messageObserver)
+        {
+            messageObservable.Subscribe<Transmit<T>>((m, t) => messageObserver.Invoke(m, t));
+        }
+
+        public static void SubscribeReceiver<T>(this IMessageObservable messageObservable, Action<T> messageObserver)
+        {
+            messageObservable.Subscribe<Received<T>>(m => messageObserver.Invoke(m));
+        }
+
+        public static void SubscribeReceiver<T>(this IMessageObservable messageObservable, AsyncAction<T> messageObserver)
+        {
+            messageObservable.Subscribe<Received<T>>((m, t) => messageObserver.Invoke(m, t));
+        }
+    }
+}
