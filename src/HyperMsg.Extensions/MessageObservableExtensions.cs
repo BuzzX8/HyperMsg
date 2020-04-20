f5efd271830d67cc6722df26543ee91129ad15ac
BuzzX8 <maxim.chasnik@gmail.com>
2020-04-20 14:01:18 +0300

    wip

--- src/HyperMsg.Extensions/MessageObservableExtensions.cs
+++ src/HyperMsg.Extensions/MessageObservableExtensions.cs
@@ -14,6 +14,16 @@ namespace HyperMsg
             return messageObservable.Subscribe<Transmit<T>>((m, t) => messageObserver.Invoke(m, t));
         }
 
+        public static IDisposable OnBufferDataTransmit(this IMessageObservable messageObservable, Action<IBuffer> messageObserver)
+        {
+            return messageObservable.OnTransmit(messageObserver);
+        }
+
+        public static IDisposable OnBufferDataTransmit(this IMessageObservable messageObservable, AsyncAction<IBuffer> messageObserver)
+        {
+            return messageObservable.OnTransmit(messageObserver);
+        }
+
         public static IDisposable OnReceived<T>(this IMessageObservable messageObservable, Action<T> messageObserver)
         {
             return messageObservable.Subscribe<Received<T>>(m => messageObserver.Invoke(m));
@@ -22,6 +32,16 @@ namespace HyperMsg
         public static IDisposable OnReceived<T>(this IMessageObservable messageObservable, AsyncAction<T> messageObserver)
         {
             return messageObservable.Subscribe<Received<T>>((m, t) => messageObserver.Invoke(m, t));
+        }
+
+        public static IDisposable OnBufferReceivedData(this IMessageObservable messageObservable, Action<IBuffer> messageObserver)
+        {
+            return messageObservable.OnReceived(messageObserver);
+        }
+
+        public static IDisposable OnBufferReceivedData(this IMessageObservable messageObservable, AsyncAction<IBuffer> messageObserver)
+        {
+            return messageObservable.OnReceived(messageObserver);
         }
     }
 }
