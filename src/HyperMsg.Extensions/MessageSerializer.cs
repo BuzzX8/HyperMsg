081b77b4f333fe5c72e3488a76e8017236ea41ec
BuzzX8 <maxim.chasnik@gmail.com>
2020-04-20 16:57:36 +0300

    wip

---
+++ src/HyperMsg.Extensions/MessageSerializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HyperMsg
+{
+    internal class MessageSerializer<T> : IDisposable
+    {
+        private readonly IMessageSender messageSender;
+        private readonly IBuffer transmittingBuffer;
+        private readonly Action<IBufferWriter<byte>, T> serializer;
+        private readonly IDisposable subscription;
+
+        internal MessageSerializer(IMessagingContext context, IBuffer transmittingBuffer, Action<IBufferWriter<byte>, T> serializer)
+        {
+            messageSender = context.Sender;
+            this.transmittingBuffer = transmittingBuffer;
+            this.serializer = serializer;
+            subscription = context.Observable.OnTransmit<T>(Handle);
+        }
+
+        private void Handle(T message)
+        {
+            serializer.Invoke(transmittingBuffer.Writer, message);
+            messageSender.TransmitBufferData(transmittingBuffer);
+        }
+
+        public void Dispose() => subscription.Dispose();
+    }
+}
