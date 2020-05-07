b4693ed1cd4aadcec0ab3d098b2860d314eccbba
BuzzX8 <maxim.chasnik@gmail.com>
2020-05-07 19:43:18 +0300

    wip

---
+++ src/HyperMsg.Extensions/MessagingObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HyperMsg
+{
+    public abstract class MessagingObject : IDisposable
+    {
+        private readonly IMessagingContext messagingContext;
+        private readonly List<IDisposable> subscriptions;
+
+        protected MessagingObject(IMessagingContext messagingContext)
+        {
+            this.messagingContext = messagingContext ?? throw new ArgumentNullException(nameof(messagingContext));
+            subscriptions = new List<IDisposable>();
+        }
+
+        protected IMessageSender Sender => messagingContext.Sender;
+
+        protected IMessageObservable Observable => messagingContext.Observable;
+
+        protected void RegisterHandler<T>(Action<T> handler) => subscriptions.Add(Observable.Subscribe(handler));
+
+        protected void RegisterHandler<T>(AsyncAction<T> handler) => subscriptions.Add(Observable.Subscribe(handler));
+
+        protected void RegisterReceiveHandler<T>(Action<T> handler) => subscriptions.Add(Observable.OnReceived(handler));
+
+        protected void RegisterReceiveHandler<T>(AsyncAction<T> handler) => subscriptions.Add(Observable.OnReceived(handler));
+
+        protected void RegisterTransmitHandler<T>(Action<T> handler) => subscriptions.Add(Observable.OnTransmit(handler));
+
+        protected void RegisterTransmitHandler<T>(AsyncAction<T> handler) => subscriptions.Add(Observable.OnTransmit(handler));
+
+        public virtual void Dispose() => subscriptions.ForEach(d => d.Dispose());
+    }
+}
--- src/HyperMsg.Extensions/MessagingTaskBase.cs
+++ src/HyperMsg.Extensions/MessagingTaskBase.cs
@@ -1,41 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 
 namespace HyperMsg
 {
-    public abstract class MessagingTaskBase : IDisposable
+    public abstract class MessagingTaskBase : MessagingObject
     {
-        private readonly IMessagingContext messagingContext;
-        private readonly List<IDisposable> subscriptions;
         private readonly IDisposable cancelSubscription;
 
-        protected MessagingTaskBase(IMessagingContext messagingContext, CancellationToken cancellationToken)
-        {
-            this.messagingContext = messagingContext ?? throw new ArgumentNullException(nameof(messagingContext));
+        protected MessagingTaskBase(IMessagingContext messagingContext, CancellationToken cancellationToken) : base(messagingContext)
+        {            
             CancellationToken = cancellationToken;
-            subscriptions = new List<IDisposable>();            
             cancelSubscription = cancellationToken.Register(Dispose);
         }
 
         protected CancellationToken CancellationToken { get; }
 
-        protected IMessageSender Sender => messagingContext.Sender;
-
-        protected IMessageObservable Observable => messagingContext.Observable;
-
-        protected void RegisterHandler<T>(Action<T> handler) => subscriptions.Add(Observable.Subscribe(handler));
-
-        protected void RegisterHandler<T>(AsyncAction<T> handler) => subscriptions.Add(Observable.Subscribe(handler));
-
-        protected void RegisterReceiveHandler<T>(Action<T> handler) => subscriptions.Add(Observable.OnReceived(handler));
-
-        protected void RegisterReceiveHandler<T>(AsyncAction<T> handler) => subscriptions.Add(Observable.OnReceived(handler));
-
-        public void Dispose()
+        public override void Dispose()
         {
-            cancelSubscription?.Dispose();
-            subscriptions.ForEach(d => d.Dispose());
+            base.Dispose();
+            cancelSubscription?.Dispose();            
         }
     }
 }
