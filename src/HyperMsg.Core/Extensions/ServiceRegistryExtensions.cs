d934084d6dd5e0573c0aa14998bf7bca729958ec
Max Gunther <maxim.chasnik@outlook.com>
2020-12-18 21:56:09 +0200

    wip

--- src/HyperMsg.Core/Extensions/ServiceRegistryExtensions.cs
+++ src/HyperMsg.Core/Extensions/ServiceRegistryExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using HyperMsg.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 using System.Buffers;
 
 namespace HyperMsg.Extensions
@@ -9,64 +10,64 @@ namespace HyperMsg.Extensions
         /// Adds core services required for messaging and buffering infrastructure (MessageSender, MessageObservable,
         /// receiving and transmitting buffer).
         /// </summary>
-        /// <param name="serviceRegistry"></param>
+        /// <param name="services"></param>
         /// <param name="receivingBufferSize">Size of receiving buffer.</param>
         /// <param name="transmittingBufferSize">Size of transmitting buffer.</param>
-        public static void AddCoreServices(this IServiceRegistry serviceRegistry, int receivingBufferSize, int transmittingBufferSize)
+        public static void AddCoreServices(this IServiceCollection services, int receivingBufferSize, int transmittingBufferSize)
         {
-            serviceRegistry.AddSharedMemoryPool();
-            serviceRegistry.AddBufferContext(receivingBufferSize, transmittingBufferSize);
-            serviceRegistry.AddMessageBroker();
+            services.AddSharedMemoryPool();
+            services.AddBufferContext(receivingBufferSize, transmittingBufferSize);
+            services.AddMessageBroker();            
         }
 
         /// <summary>
         /// Adds shared MemoryPool<byte> as service.
         /// </summary>
-        /// <param name="serviceRegistry"></param>
-        public static void AddSharedMemoryPool(this IServiceRegistry serviceRegistry)  {}// serviceRegistry.AddService(MemoryPool<byte>.Shared);
+        /// <param name="services"></param>
+        public static void AddSharedMemoryPool(this IServiceCollection services) => services.AddSingleton(MemoryPool<byte>.Shared);
 
         /// <summary>
         /// Adds implementations for IBufferContext. Depends on MemoryPool<byte>.
         /// </summary>
-        /// <param name="serviceRegistry"></param>
+        /// <param name="services"></param>
         /// <param name="receivingBufferSize">Size of receiving buffer.</param>
         /// <param name="transmittingBufferSize">Size of transmitting buffer.</param>
-        public static void AddBufferContext(this IServiceRegistry serviceRegistry, int receivingBufferSize, int transmittingBufferSize)
+        public static void AddBufferContext(this IServiceCollection services, int receivingBufferSize, int transmittingBufferSize)
         {
-            //serviceRegistry.AddService(provider =>
-            //{
-            //    var memoryPool = provider.GetRequiredService<MemoryPool<byte>>();
+            services.AddSingleton(provider =>
+            {
+                var memoryPool = provider.GetRequiredService<MemoryPool<byte>>();
 
-            //    var receivingBuffer = new Buffer(memoryPool.Rent(receivingBufferSize));
-            //    var transmittingBuffer = new Buffer(memoryPool.Rent(transmittingBufferSize));
+                var receivingBuffer = new Buffer(memoryPool.Rent(receivingBufferSize));
+                var transmittingBuffer = new Buffer(memoryPool.Rent(transmittingBufferSize));
 
-            //    return new BufferContext(receivingBuffer, transmittingBuffer) as IBufferContext;
-            //});
+                return new BufferContext(receivingBuffer, transmittingBuffer) as IBufferContext;
+            });
         }
 
         /// <summary>
         /// Adds implementation for IBufferFactory. Depends on MemoryPool<byte>
         /// </summary>
-        /// <param name="serviceRegistry"></param>
-        public static void AddBufferFactory(this IServiceRegistry serviceRegistry)
+        /// <param name="services"></param>
+        public static void AddBufferFactory(this IServiceCollection services)
         {
-            ///*serviceRegistry*/.AddService(provider =>
-            //{
-            //    var memoryPool = provider.GetRequiredService<MemoryPool<byte>>();
-            //    return new BufferFactory(memoryPool) as IBufferFactory;
-            //});
+            services.AddSingleton(provider =>
+            {
+                var memoryPool = provider.GetRequiredService<MemoryPool<byte>>();
+                return new BufferFactory(memoryPool) as IBufferFactory;
+            });
         }
 
         /// <summary>
         /// Adds implementation for services IMessagingContext, IMessageSender and IMessageObservable.
         /// </summary>
         /// <param name="serviceRegistry"></param>
-        public static void AddMessageBroker(this IServiceRegistry serviceRegistry)
+        public static void AddMessageBroker(this IServiceCollection services)
         {
-            //var broker = new MessageBroker();
-            //serviceRegistry.AddService<IMessageSender>(broker);
-            //serviceRegistry.AddService<IMessageObservable>(broker);
-            //serviceRegistry.AddService<IMessagingContext>(broker);
+            var broker = new MessageBroker();
+            services.AddSingleton<IMessageSender>(broker);
+            services.AddSingleton<IMessageObservable>(broker);
+            services.AddSingleton<IMessagingContext>(broker);
         }
     }
 }
--- test/HyperMsg.Core.Tests/Extensions/ServiceRegistryExtensionsTests.cs
+++ test/HyperMsg.Core.Tests/Extensions/ServiceRegistryExtensionsTests.cs
@@ -1,25 +1,61 @@
-﻿using FakeItEasy;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Buffers;
 using Xunit;
 
 namespace HyperMsg.Extensions
 {
     public class ServiceRegistryExtensionsTests
     {
-        private readonly IServiceRegistry serviceRegistry = A.Fake<IServiceRegistry>();
+        private readonly ServiceCollection services = new ServiceCollection();
 
         [Fact]
-        public void AddService_Registers_Service_Instance()
+        public void AddMessageBroker_Adds_Sender_Observable_And_Context()
         {
-            var actual = default(object);
-            var expected = A.Fake<IMessageSender>();
-            A.CallTo(() => serviceRegistry.Add(typeof(IMessageSender), A<object>._)).Invokes(foc =>
-            {
-                actual = foc.GetArgument<object>(1);
-            });
+            services.AddMessageBroker();
+            var provider = services.BuildServiceProvider();
 
-            serviceRegistry.AddService(expected);
+            var sender = provider.GetService<IMessageSender>();
+            var observable = provider.GetService<IMessageObservable>();
+            var context = provider.GetService<IMessagingContext>();
 
-            Assert.Same(expected, actual);
+            Assert.NotNull(sender);
+            Assert.NotNull(observable);
+            Assert.NotNull(context);
+        }
+
+        [Fact]
+        public void AddSharedMemoryPool_Adds_Memory_Pool()
+        {
+            services.AddSharedMemoryPool();
+            var provider = services.BuildServiceProvider();
+
+            var pool = provider.GetService<MemoryPool<byte>>();
+
+            Assert.NotNull(pool);
+        }
+
+        [Fact]
+        public void AddBufferContext_Adds_BufferContext()
+        {
+            services.AddSharedMemoryPool();
+            services.AddBufferContext(100, 100);
+            var provider = services.BuildServiceProvider();
+
+            var context = provider.GetService<IBufferContext>();
+
+            Assert.NotNull(context);
+        }
+
+        [Fact]
+        public void AddBufferFactory_Adds_BufferFactory()
+        {
+            services.AddSharedMemoryPool();
+            services.AddBufferFactory();
+            var provider = services.BuildServiceProvider();
+
+            var factory = provider.GetService<IBufferFactory>();
+
+            Assert.NotNull(factory);
         }
     }
 }
