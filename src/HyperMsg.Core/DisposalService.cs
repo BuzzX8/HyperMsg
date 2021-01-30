10201554938c14a4590919095745e2a782f8d5ac
Max Gunther <maxim.chasnik@gmail.com>
2021-01-30 20:43:09 +0200

    Handler registration extensions (#115)

---
+++ src/HyperMsg.Core/DisposalService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HyperMsg
+{
+    internal class DisposalService : IDisposable
+    {
+        private readonly List<IDisposable> disposables = new();
+
+        internal void AddDisposable(IDisposable disposable) => disposables.Add(disposable);
+
+        public void Dispose() => disposables.ForEach(d => d.Dispose());
+    }
+}
