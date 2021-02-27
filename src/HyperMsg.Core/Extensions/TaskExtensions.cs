cbeffb912f887e0288dbdd456e0ecffdb8b20cab
Max Gunther <maxim.chasnik@outlook.com>
2021-02-27 18:22:07 +0200

    wip

---
+++ src/HyperMsg.Core/Extensions/TaskExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HyperMsg.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static Task OnSuccessfullyComplete(this Task task, Action completeHandler)
+        {
+            return task.ContinueWith(t => completeHandler.Invoke(), TaskContinuationOptions.NotOnFaulted | TaskContinuationOptions.NotOnCanceled);
+        }
+
+        public static Task OnSuccessfullyComplete(this Task task, Func<Task> completeHandler)
+        {
+            return task.ContinueWith(async _ => await completeHandler.Invoke(), TaskContinuationOptions.NotOnFaulted | TaskContinuationOptions.NotOnCanceled);
+        }
+
+        public static Task OnSuccessfullyComplete<T>(this Task<T> task, Action<T> completeHandler)
+        {
+            return task.ContinueWith(t => completeHandler.Invoke(t.Result), TaskContinuationOptions.NotOnFaulted | TaskContinuationOptions.NotOnCanceled);
+        }
+
+        public static Task OnFault(this Task task, Action<IReadOnlyList<Exception>> faultHandler)
+        {
+            return task.ContinueWith(t =>
+            {
+                t.Exception.Flatten();
+                faultHandler.Invoke(t.Exception.InnerExceptions);
+            }, TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
+}
---
+++ test/HyperMsg.Core.Tests/Extensions/TaskExtensionsTests.cs
@@ -0,0 +1,53 @@
+﻿using FakeItEasy;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HyperMsg.Extensions
+{
+    public class TaskExtensionsTests
+    {
+        [Fact]
+        public async Task OnSuccessfullyComplete_Invokes_CompleteHandler_For_Completed_Task()
+        {
+            var completeHandler = A.Fake<Action>();
+
+            await Task.CompletedTask.OnSuccessfullyComplete(completeHandler);
+
+            A.CallTo(() => completeHandler.Invoke()).MustHaveHappened();
+        }
+
+        [Fact]
+        public async Task OnSuccessfullyComplete_Does_Not_Invokes_CompleteHandler_For_Faulted_Task()
+        {
+            var completeHandler = A.Fake<Action>();
+
+            await Assert.ThrowsAsync<TaskCanceledException>(() =>Task.FromException(new Exception()).OnSuccessfullyComplete(completeHandler));
+
+            A.CallTo(() => completeHandler.Invoke()).MustNotHaveHappened();
+        }
+
+        [Fact]
+        public async Task OnSuccessfullyComplete_Does_Not_Invokes_CompleteHandler_For_Cancelled_Task()
+        {
+            var tokenSource = new CancellationTokenSource();
+            var completeHandler = A.Fake<Action>();
+
+            await Assert.ThrowsAsync<TaskCanceledException>(() => Task.FromCanceled(new(true)).OnSuccessfullyComplete(completeHandler));
+
+            A.CallTo(() => completeHandler.Invoke()).MustNotHaveHappened();
+        }
+
+        [Fact]
+        public async Task OnSuccessfullyComplete_Invokes_CompleteHandler_For_Completed_Task_With_Result()
+        {
+            var completeHandler = A.Fake<Action<Guid>>();
+            var result = Guid.NewGuid();
+
+            await Task.FromResult(result).OnSuccessfullyComplete(completeHandler);
+
+            A.CallTo(() => completeHandler.Invoke(result)).MustHaveHappened();
+        }
+    }
+}
