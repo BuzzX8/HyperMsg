f5b25eb60ceaf59fcfdb7888a47b5e82124f219e
Max Gunther <maxim.chasnik@gmail.com>
2021-03-13 10:43:48 +0200

    wip

---
+++ src/HyperMsg.TestBase/TestBase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace HyperMsg.TestBase
+{
+    public abstract class TestBase : IDisposable
+    {
+        private readonly ServiceHost serviceHost;
+
+        protected TestBase(Action<IServiceCollection> serviceConfigurator = null)
+        {
+            serviceHost = ServiceHost.CreateDefault(serviceConfigurator);
+            serviceHost.Start();
+        }
+
+        protected IMessagingContext MessagingContext => GetRequiredService<IMessagingContext>();
+
+        protected IMessageSender MessageSender => GetRequiredService<IMessageSender>();
+
+        protected IMessageHandlersRegistry HandlersRegistry => GetRequiredService<IMessageHandlersRegistry>();
+
+        protected T GetService<T>() => serviceHost.GetService<T>();
+
+        protected T GetRequiredService<T>() => serviceHost.GetRequiredService<T>();
+
+        public virtual void Dispose()
+        {
+            serviceHost.Stop();
+            serviceHost.Dispose();
+        }
+    }
+}
