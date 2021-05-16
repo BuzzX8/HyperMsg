4402cd66c7c217405517542f15e65d1dd1a1876b
Max Gunther <maxim.chasnik@gmail.com>
2021-05-16 15:42:55 +0300

    wip

--- src/HyperMsg.Core/BufferReader.cs
+++ src/HyperMsg.Core/BufferReader.cs
@@ -1,6 +1,10 @@
 ﻿using System.Buffers;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HyperMsg
 {
     public delegate int BufferReader(ReadOnlySequence<byte> buffer);
+
+    public delegate Task<int> AsyncBufferReader(ReadOnlySequence<byte> buffer, CancellationToken cancellationToken);
 }
