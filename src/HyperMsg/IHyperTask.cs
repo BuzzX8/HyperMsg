a36e5e521dc426639dda9b87149dfa03176bf066
Max Gunther <maxim.chasnik@gmail.com>
2021-05-31 15:28:01 +0300

    wip

--- src/HyperMsg/IHyperTask.cs
+++ src/HyperMsg/IHyperTask.cs
@@ -3,12 +3,12 @@ using System.Threading.Tasks;
 
 namespace HyperMsg
 {
-    public interface IMessagingTask : IDisposable
+    public interface IHyperTask : IDisposable
     {
         Task Completion { get; }
     }
 
-    public interface IMessagingTask<T> : IDisposable
+    public interface IHyperTask<T> : IDisposable
     {
         Task<T> Completion { get; }
     }
