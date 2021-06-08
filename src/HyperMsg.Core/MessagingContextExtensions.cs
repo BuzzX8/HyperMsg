4146b9f959ef2fc59c7d8c7e791af70e9836d221
Max Gunther <maxim.chasnik@gmail.com>
2021-06-08 22:25:52 +0300

    Removed redundant type parameter

--- src/HyperMsg.Core/MessagingContextExtensions.cs
+++ src/HyperMsg.Core/MessagingContextExtensions.cs
@@ -10,9 +10,9 @@ namespace HyperMsg
             messagingContext.SendAndWaitMessage<TRequest, TResponse>(request, _ => true, cancellationToken);
 
         public static Task<TResponse> SendAndWaitMessage<TRequest, TResponse>(this IMessagingContext messagingContext, TRequest request, Func<TResponse, bool> responsePredicate, CancellationToken cancellationToken = default) => 
-            messagingContext.SendAndWaitMessage<TRequest, TResponse>((sender, token) => sender.SendAsync(request, token), responsePredicate, cancellationToken);
+            messagingContext.SendAndWaitMessage((sender, token) => sender.SendAsync(request, token), responsePredicate, cancellationToken);
 
-        public static async Task<TResponse> SendAndWaitMessage<TRequest, TResponse>(this IMessagingContext messagingContext, AsyncAction<IMessageSender> sendAction, Func<TResponse, bool> responsePredicate, CancellationToken cancellationToken = default)
+        public static async Task<TResponse> SendAndWaitMessage<TResponse>(this IMessagingContext messagingContext, AsyncAction<IMessageSender> sendAction, Func<TResponse, bool> responsePredicate, CancellationToken cancellationToken = default)
         {
             var waitTask = messagingContext.HandlersRegistry.WaitMessage(responsePredicate, cancellationToken);
             await sendAction.Invoke(messagingContext.Sender, cancellationToken);
