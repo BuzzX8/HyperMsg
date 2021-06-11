8d491eba3cd9c967cdeea703fd4c11bd0a928e73
Max Gunther <maxim.chasnik@gmail.com>
2021-06-11 21:42:48 +0300

    wip

---
+++ src/HyperMsg.Core/Messages/PipeMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace HyperMsg.Messages
+{
+    internal class PipeMessage<T>
+    {
+        public PipeMessage(object pipeId, object portId, T message) => 
+            (PipeId, PortId, Message) = (pipeId, portId, message);
+
+        public object PipeId { get; }
+
+        public object PortId { get; }
+
+        public T Message { get; }
+    }
+}
--- test/HyperMsg.Core.Tests/MessageBrokerExtensionTests.cs
+++ test/HyperMsg.Core.Tests/MessageBrokerExtensionTests.cs
@@ -145,5 +145,75 @@ namespace HyperMsg
 
             Assert.Equal(message, actualMessage);
         }
+
+        private readonly Guid pipeId = Guid.NewGuid();
+        private readonly Guid portId = Guid.NewGuid();
+        private readonly Guid message = Guid.NewGuid();
+
+        [Fact]
+        public void SendToPipe_Invokes_Handler_Registered_With_RegisterPipeHandler()
+        {
+            var filter = A.Fake<Action<Guid>>();
+            broker.RegisterPipeHandler(pipeId, portId, filter);
+
+            broker.SendToPipe(pipeId, portId, message);
+
+            A.CallTo(() => filter.Invoke(message)).MustHaveHappened();
+        }
+
+        [Fact]
+        public async Task SendToPipeAsync_Invokes_Handler_Registered_With_RegisterPipeHandler()
+        {
+            var filter = A.Fake<AsyncAction<Guid>>();
+            broker.RegisterPipeHandler(pipeId, portId, filter);
+
+            await broker.SendToPipeAsync(pipeId, portId, message);
+
+            A.CallTo(() => filter.Invoke(message, A<CancellationToken>._)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void SendToPipe_Does_Not_Invokes_Pipe_Handler_With_Different_PortId()
+        {
+            var filter = A.Fake<Action<Guid>>();
+            broker.RegisterPipeHandler(pipeId, portId, filter);
+
+            broker.SendToPipe(pipeId, Guid.NewGuid(), message);
+
+            A.CallTo(() => filter.Invoke(message)).MustNotHaveHappened();
+        }
+
+        [Fact]
+        public async Task SendToPipeAsync_Does_Not_Invokes_Pipe_Handler_With_Different_PortId()
+        {
+            var filter = A.Fake<AsyncAction<Guid>>();
+            broker.RegisterPipeHandler(pipeId, portId, filter);
+
+            await broker.SendToPipeAsync(pipeId, Guid.NewGuid(), message);
+
+            A.CallTo(() => filter.Invoke(message, A<CancellationToken>._)).MustNotHaveHappened();
+        }
+
+        [Fact]
+        public void SendToPipe_Does_Not_Invokes_Pipe_Handler_With_Different_PipeId()
+        {
+            var filter = A.Fake<Action<Guid>>();
+            broker.RegisterPipeHandler(pipeId, portId, filter);
+
+            broker.SendToPipe(Guid.NewGuid(), portId, message);
+
+            A.CallTo(() => filter.Invoke(message)).MustNotHaveHappened();
+        }
+
+        [Fact]
+        public async Task SendToPipeAsync_Does_Not_Invokes_Pipe_Handler_With_Different_PipeId()
+        {
+            var filter = A.Fake<AsyncAction<Guid>>();
+            broker.RegisterPipeHandler(pipeId, portId, filter);
+
+            await broker.SendToPipeAsync(Guid.NewGuid(), portId, message);
+
+            A.CallTo(() => filter.Invoke(message, A<CancellationToken>._)).MustNotHaveHappened();
+        }
     }
 }
