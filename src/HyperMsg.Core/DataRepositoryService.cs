3887c47b3bc3d24eaaea91727ae8293db5617230
Max Gunther <maxim.chasnik@gmail.com>
2021-06-16 16:34:00 +0300

    Data repository service (#153)

---
+++ src/HyperMsg.Core/DataRepositoryService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HyperMsg
+{
+    internal class DataRepositoryService : MessagingService
+    {
+        private IDataRepository dataRepository;
+
+        public DataRepositoryService(IMessagingContext messagingContext, IDataRepository dataRepository) : base(messagingContext) =>
+            this.dataRepository = dataRepository;
+
+        protected override IEnumerable<IDisposable> GetAutoDisposables()
+        {
+            yield return this.RegisterRequestHandler(() => dataRepository);
+        }
+    }
+}
--- test/HyperMsg.Core.Tests/DataRepositoryExtensionsTests.cs
+++ test/HyperMsg.Core.Tests/DataRepositoryExtensionsTests.cs
@@ -5,18 +5,18 @@ namespace HyperMsg
 {
     public class DataRepositoryExtensionsTests : ServiceHostFixture
     {
-        private readonly IDataRepository settings;
+        private readonly IDataRepository repository;
 
-        public DataRepositoryExtensionsTests() => settings = GetRequiredService<IDataRepository>();
+        public DataRepositoryExtensionsTests() => repository = GetRequiredService<IDataRepository>();
 
         [Fact]
         public void Get_Returns_Value_Provided_With_Set()
         {
             var value = Guid.NewGuid();
 
-            settings.AddOrUpdate(value);
+            repository.AddOrReplace(value);
 
-            var actualValue = settings.Get<Guid>();
+            var actualValue = repository.Get<Guid>();
 
             Assert.Equal(value, actualValue);
         }
@@ -24,7 +24,54 @@ namespace HyperMsg
         [Fact]
         public void TryGet_Returns_False_For_Not_Existing_Value()
         {
-            Assert.False(settings.TryGet<string>(Guid.NewGuid().ToString(), out _));
+            Assert.False(repository.TryGet<string>(Guid.NewGuid().ToString(), out _));
+        }
+
+        [Fact]
+        public void SendDataRequest_Returns_Value_Stored_In_Repository()
+        {
+            var value = Guid.NewGuid();
+            var key = value.ToString();
+
+            repository.AddOrReplace(key, value);
+            var actualValue = MessageSender.SendDataRequest<Guid>(key);
+
+            Assert.Equal(value, actualValue);
+        }
+
+        [Fact]
+        public void SendToDataRepository_Adds_Value_To_Data_Repository()
+        {
+            var value = Guid.NewGuid();
+            var key = value.ToString();
+                        
+            MessageSender.SendToDataRepository(key, value);
+
+            Assert.True(repository.Contains<Guid>(key));
+            Assert.Equal(value, repository.Get<Guid>(key));
+        }
+
+        [Fact]
+        public void SendDataExistenceRequest_Returns_True_Id_Data_Present_In_Repository()
+        {
+            var value = Guid.NewGuid();
+            var key = value.ToString();
+
+            repository.AddOrReplace(key, value);
+
+            Assert.True(MessageSender.SendDataExistenceRequest<Guid>(key));
+        }
+
+        [Fact]
+        public void SendDataDeletionRequest_Removes_Data_From_Repository()
+        {
+            var value = Guid.NewGuid();
+            var key = value.ToString();
+            repository.AddOrReplace(key, value);
+
+            MessageSender.SendDataDeletionRequest<Guid>(key);
+
+            Assert.False(repository.Contains<Guid>(key));
         }
     }
 }
