c6d2e6f81ed1f9067d942ce7e1a73816c10c44bc
Max Gunther <maxim.chasnik@gmail.com>
2021-07-01 16:07:27 +0300

    wip

--- test/HyperMsg.Core.Tests/PipeFilterTests.cs
+++ test/HyperMsg.Core.Tests/PipeFilterTests.cs
@@ -1,26 +1,93 @@
 ﻿using FakeItEasy;
 using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace HyperMsg
 {
-    public class PipeFilterTests
+    public class PipeFilterTests : ServiceHostFixture
     {
+        //private readonly IMessageSender messageSender;
+        private readonly PipeFilter<string> pipeFilter;
+        private readonly Func<object, object, string, bool> filterFunc;
+
+        private readonly Guid pipeId = Guid.NewGuid();
+        private readonly Guid portId = Guid.NewGuid();
+        private readonly string message = Guid.NewGuid().ToString();
+
+        public PipeFilterTests()
+        {
+            //messageSender = A.Fake<IMessageSender>();
+            filterFunc = A.Fake<Func<object, object, string, bool>>();
+            pipeFilter = new PipeFilter<string>(MessageSender, filterFunc);
+        }
+
         [Fact]
         public void SendToPipe_Extension_Invokes_Filter_Predicate()
         {
-            var messageSender = A.Fake<IMessageSender>();
-            var filterFunc = A.Fake<Func<object, object, string, bool>>();
-
-            var pipeId = Guid.NewGuid();
-            var portId = Guid.NewGuid();
-            var message = Guid.NewGuid().ToString();
-
-            var filter = new PipeFilter<string>(messageSender, filterFunc);
-
-            filter.SendToPipe(pipeId, portId, message);
+            pipeFilter.SendToPipe(pipeId, portId, message);
 
             A.CallTo(() => filterFunc.Invoke(pipeId, portId, message)).MustHaveHappened();
+        }
+
+        [Fact]
+        public async Task SendToPipeAsync_Extension_Invokes_Filter_Predicate()
+        {
+            await pipeFilter.SendToPipeAsync(pipeId, portId, message);
+
+            A.CallTo(() => filterFunc.Invoke(pipeId, portId, message)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void SendToPipe_Extension_Does_Not_Sends_Message_If_FilterFunc_Returns_False()
+        {
+            A.CallTo(() => filterFunc.Invoke(pipeId, portId, message)).Returns(false);
+
+            var actualMessage = string.Empty;
+            HandlersRegistry.RegisterPipeHandler<string>(pipeId, portId, message => actualMessage = message);
+
+            pipeFilter.SendToPipe(pipeId, portId, message);
+
+            Assert.Empty(actualMessage);
+        }
+
+        [Fact]
+        public async Task SendToPipeAsync_Extension_Does_Not_Sends_Message_If_FilterFunc_Returns_False()
+        {
+            A.CallTo(() => filterFunc.Invoke(pipeId, portId, message)).Returns(false);
+
+            var actualMessage = string.Empty;
+            HandlersRegistry.RegisterPipeHandler<string>(pipeId, portId, message => actualMessage = message);
+
+            await pipeFilter.SendToPipeAsync(pipeId, portId, message);
+
+            Assert.Empty(actualMessage);
+        }
+
+        [Fact]
+        public void SendToPipe_Extension_Sends_Message_If_FilterFunc_Returns_True()
+        {
+            A.CallTo(() => filterFunc.Invoke(pipeId, portId, message)).Returns(true);
+
+            var actualMessage = string.Empty;
+            HandlersRegistry.RegisterPipeHandler<string>(pipeId, portId, message => actualMessage = message);
+
+            pipeFilter.SendToPipe(pipeId, portId, message);
+
+            Assert.Equal(message, actualMessage);
+        }
+
+        [Fact]
+        public async Task SendToPipeAsync_Extension_Message_If_FilterFunc_Returns_True()
+        {
+            A.CallTo(() => filterFunc.Invoke(pipeId, portId, message)).Returns(true);
+
+            var actualMessage = string.Empty;
+            HandlersRegistry.RegisterPipeHandler<string>(pipeId, portId, message => actualMessage = message);
+
+            await pipeFilter.SendToPipeAsync(pipeId, portId, message);
+
+            Assert.Equal(message, actualMessage);
         }
     }
 }
