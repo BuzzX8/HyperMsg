c55441446144e47cc46e2e8dec6567c372a60dcb
Max Gunther <maxim.chasnik@gmail.com>
2021-08-04 19:11:23 +0300

    wip

---
+++ src/HyperMsg.Core/MessagingObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HyperMsg
+{
+    public class MessagingObject : MessagingContextProxy, INotifyPropertyChanged, IDisposable
+    {
+        private readonly List<IDisposable> registrations = new();
+
+        public MessagingObject(IMessagingContext messagingContext) : base(messagingContext)
+        { }
+
+        protected void AddRegistration(IDisposable registration) => registrations.Add(registration);
+
+        protected void AddRegistrations(params IDisposable[] registrations) => this.registrations.AddRange(registrations);
+
+        public void Dispose() => registrations.ForEach(r => r.Dispose());
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ test/HyperMsg.Core.Tests/MessagingObjectTests.cs
@@ -0,0 +1,21 @@
+﻿namespace HyperMsg
+{
+    public class MessagingObjectTests
+    {
+        private readonly MessageBroker messageBroker;
+        private readonly MessagingObjectImpl messagingObject;
+
+        public MessagingObjectTests()
+        {
+            messageBroker = new();
+            messagingObject = new(messageBroker);
+        }
+    }
+
+    internal class MessagingObjectImpl : MessagingObject
+    {
+        public MessagingObjectImpl(IMessagingContext messagingContext) : base(messagingContext)
+        {
+        }
+    }
+}
