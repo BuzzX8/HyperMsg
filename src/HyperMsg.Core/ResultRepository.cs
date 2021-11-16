ec8acf8c93b9b03ae4ae7e451e9c53f0827d02cd
Maxim <maxim.chasnik@gmail.com>
2021-11-16 18:29:35 +0200

    wip

---
+++ src/HyperMsg.Core/ResultRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HyperMsg
+{
+    internal class ResultRepository
+    {
+        public Task<T> GetResultPromise<T>(object key, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult<T>(default);
+        }
+
+        public void SetResult<T>(object key, T result)
+        {
+
+        }
+    }
+}
