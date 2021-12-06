8b89ae73cfe3e341d3eadf03d383cc37ff4b540c
Maxim <maxim.chasnik@gmail.com>
2021-12-06 17:32:00 +0200

    wip

--- src/HyperMsg.Core/ServiceCollectionExtensions.cs
+++ src/HyperMsg.Core/ServiceCollectionExtensions.cs
@@ -21,6 +21,7 @@ public static class ServiceCollectionExtensions
             .AddMessageBroker()
             .AddSerializationFilter()
             .AddHandlersRegistration()
+            .AddSingleton<IResultRepository, ResultRepository>()
             .WireBaseInterfaces();
     }
 
