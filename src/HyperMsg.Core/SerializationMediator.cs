16c6bcde5c3a2da8f4344c6ef422183db42e3c47
Maxim <maxim.chasnik@gmail.com>
2021-12-08 12:07:47 +0200

    wip

---
+++ src/HyperMsg.Core/SerializationMediator.cs
@@ -0,0 +1,38 @@
+﻿namespace HyperMsg;
+
+public class SerializationMediator : ISender
+{
+    private readonly IBuffer buffer;
+    private readonly ISerializersRegistry registry;
+
+    public void Send<T>(T message) => SerializeMessage(message);
+
+    public Task SendAsync<T>(T message, CancellationToken _)
+    {
+        SerializeMessage(message);
+
+        return Task.CompletedTask;
+    }
+
+    private void SerializeMessage<T>(in T message)
+    {
+        if (!TrySerialize(message))
+        {
+            throw new InvalidOperationException();
+        }
+    }
+
+    private bool TrySerialize<T>(in T message)
+    {
+        if (!registry.Contains<T>())
+        {
+            return false;
+        }
+
+        var serializer = registry.Get<T>();
+
+        serializer.Invoke(buffer.Writer, message);
+
+        return true;
+    }
+}
---
+++ src/HyperMsg/ISerializersRegistry.cs
@@ -0,0 +1,10 @@
+﻿namespace HyperMsg;
+
+public interface ISerializersRegistry
+{
+    void Add<T>(Action<IBufferWriter, T> serializer);
+
+    Action<IBufferWriter, T> Get<T>();
+
+    bool Contains<T>();
+}
