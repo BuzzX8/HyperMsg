9673b099ec0e956fd144808ad40fc109cbfa67b1
Maxim <maxim.chasnik@gmail.com>
2022-04-25 11:23:49 +0300

    wip

---
+++ test/HyperMsg.Core.Tests/RuntimeBuilderTests.cs
@@ -0,0 +1,32 @@
+﻿using FakeItEasy;
+using Xunit;
+
+namespace HyperMsg
+{
+    public class RuntimeBuilderTests
+    {
+        public readonly RuntimeBuilder builder = new ();
+
+        [Fact]
+        public void AddService_Adds_Service_To_Runtime_ServiceProvider()
+        {
+            var service = A.Fake<Action>();
+            builder.AddService(service);
+
+            var runtime = builder.Build();
+
+            Assert.Same(service, runtime.ServiceProvider.GetService(typeof(Action)));
+        }
+
+        [Fact]
+        public void Build_Invokes_Initializers_Added_By_AddInitializer()
+        {
+            var initializer = A.Fake<Action<IRuntime>>();
+            builder.AddInitializer(initializer);
+
+            var runtime = builder.Build();
+
+            A.CallTo(() => initializer.Invoke(runtime)).MustHaveHappened();
+        }
+    }
+}
