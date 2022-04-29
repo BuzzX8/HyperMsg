68b8b9fce238b0b135a79d5f4b3d8b2f1ce5fd81
Maxim <maxim.chasnik@gmail.com>
2022-04-29 20:28:20 +0300

    wip

--- src/HyperMsg.Core/ServiceCollectionExtensions.cs
+++ src/HyperMsg.Core/ServiceCollectionExtensions.cs
@@ -7,7 +7,7 @@ namespace HyperMsg
         public const int DefaultBufferSize = 65 * 1024;
 
         public static IServiceCollection AddContext(this IServiceCollection services) =>
-            services.AddSingleton(new Context());
+            services.AddSingleton<IContext, Context>();
 
         public static IServiceCollection AddSerializationFilter(this IServiceCollection services, int serializationBufferSize = DefaultBufferSize) =>
             services.AddSerializationFilter(BufferFactory.Shared.CreateBuffer(serializationBufferSize));
