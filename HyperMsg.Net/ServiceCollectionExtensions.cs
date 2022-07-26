de8d2654e8071d1d78319923b6ad8068f24779a4
Maxim <maxim.chasnik@gmail.com>
2022-07-26 15:49:52 +0300

    wip

--- HyperMsg.Net/ServiceCollectionExtensions.cs
+++ HyperMsg.Net/ServiceCollectionExtensions.cs
@@ -6,4 +6,6 @@ namespace HyperMsg.Net;
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddSocket(this IServiceCollection services, Func<IServiceProvider, Socket> socketFactory) => services.AddSingleton(socketFactory);
+
+    public static IServiceCollection AddClientSocketService(this IServiceCollection services) => services.AddHostedService<ClientSocketService>();
 }
