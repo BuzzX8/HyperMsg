5de205383e262bfa87119bfc926ba1f6f3df3e64
Max Gunther <maxim.chasnik@gmail.com>
2022-07-27 10:20:51 +0300

    Socket filter (#193)

---
+++ src/HyperMsg.Net/ServiceCollectionExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net.Sockets;
+
+namespace HyperMsg.Net;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSocket(this IServiceCollection services, Func<IServiceProvider, Socket> socketFactory) => services.AddSingleton(socketFactory);
+}
