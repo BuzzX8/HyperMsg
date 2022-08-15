885f720121ab1f6f4f187057c50e0690371691bb
Maxim <maxim.chasnik@gmail.com>
2022-08-15 20:23:58 +0300

    wip

--- src/HyperMsg.Net/SocketConnection.cs
+++ src/HyperMsg.Net/SocketConnection.cs
@@ -7,10 +7,35 @@ public class SocketConnection : IConnectionStateObserver
 {
     private readonly Socket socket;
     private readonly EndPoint endPoint;
+    private readonly object sync;
+
+    internal SocketConnection(Socket socket, EndPoint endPoint)
+    {
+        this.endPoint = endPoint;
+        this.socket = socket;
+        State = socket.Connected ? ConnectionState.Opened : ConnectionState.Closed;
+        sync = new();
+    }
 
     public bool IsOpen => socket.Connected;
 
     public ConnectionState State { get; private set; }
+
+    public void Open()
+    {
+        lock (sync)
+        {
+
+            if (IsOpen)
+            {
+                return;
+            }
+
+            ChangeState(ConnectionState.Opening);
+            socket.Connect(endPoint);
+            ChangeState(ConnectionState.Opened);
+        }
+    }
 
     private void ChangeState(ConnectionState state)
     {
@@ -18,7 +43,15 @@ public class SocketConnection : IConnectionStateObserver
         StateChanged?.Invoke(state);
     }
 
-    public event Action<ConnectionState> StateChanged;
+    public static SocketConnection CreateTcpConnection(IPAddress address, int port) => CreateTcpConnection(new IPEndPoint(address, port));
+
+    public static SocketConnection CreateTcpConnection(EndPoint endPoint)
+    {
+        var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        return new(socket, endPoint);
+    }
+
+    public event Action<ConnectionState>? StateChanged;
 }
 
 public interface IConnectionStateObserver
