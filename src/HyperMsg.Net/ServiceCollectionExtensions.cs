97e6704549dfd2fa56f641347f84e9b5034a8e5a
Maxim <maxim.chasnik@gmail.com>
2022-09-06 14:27:46 +0300

    Removed HyperMsg.Net

--- src/HyperMsg.Net/ServiceCollectionExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System.Net.Sockets;
-
-namespace HyperMsg.Net;
-
-public static class ServiceCollectionExtensions
-{
-    public static IServiceCollection AddSocket(this IServiceCollection services, Func<IServiceProvider, Socket> socketFactory) => services.AddSingleton(socketFactory);
-}
