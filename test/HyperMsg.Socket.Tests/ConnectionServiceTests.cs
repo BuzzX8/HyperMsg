81e3b6d9ea9b835fb29ea930b872a4e272b05d69
Maxim <maxim.chasnik@gmail.com>
2022-11-23 20:29:20 +0200

    wip

--- test/HyperMsg.Socket.Tests/ConnectionServiceTests.cs
+++ test/HyperMsg.Socket.Tests/ConnectionServiceTests.cs
@@ -24,6 +24,8 @@ public class ConnectionServiceTests : IDisposable
         syncEvent = new();
     }
 
+    #region
+
     [Fact]
     public void Dispatching_Connect_Message_Creates_Socket_Connection()
     {
@@ -47,6 +49,32 @@ public class ConnectionServiceTests : IDisposable
         acceptingTask.Wait(TimeSpan.FromSeconds(10));
         Assert.True(acceptingTask.IsCompleted);
     }
+
+    [Fact]
+    public void Dispatching_Connect_Message_For_Connected_Socket()
+    {
+        var result = default(ConnectResult);
+
+        broker.Register<ConnectResult>(c =>
+        {
+            result = c;
+            SetSyncEvent();
+        });
+
+        acceptingSocket.Bind(endPoint);
+        acceptingSocket.Listen();
+        var acceptingTask = acceptingSocket.AcceptAsync();
+
+        broker.Dispatch(new Connect(endPoint));
+        WaitSyncEvent();
+        syncEvent.Reset();
+        
+        Assert.Throws<SocketException>(() => broker.Dispatch(new Connect(endPoint)));
+    }
+
+    #endregion
+
+    #region Disconnect
 
     [Fact]
     public void Dispatching_Connect_Message_Returns_Error()
@@ -82,6 +110,8 @@ public class ConnectionServiceTests : IDisposable
 
         Assert.Equal(SocketError.NotConnected, result.Error);
     }
+
+    #endregion
 
     private void SetSyncEvent() => syncEvent.Set();
 
