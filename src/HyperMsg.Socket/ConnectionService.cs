06233c5a280932353f1e2628cb9bb2ec926fa414
Maxim <maxim.chasnik@gmail.com>
2022-11-23 14:28:17 +0200

    wip

---
+++ src/HyperMsg.Socket/ConnectionService.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace HyperMsg.Socket;
+
+public class ConnectionService
+{
+    private readonly IDispatcher dispatcher;
+    private readonly IRegistry registry;
+
+    private readonly System.Net.Sockets.Socket socket;
+    private readonly SocketAsyncEventArgs asyncEventArgs;
+
+    public ConnectionService(IDispatcher dispatcher, IRegistry registry, System.Net.Sockets.Socket socket)
+    {
+        this.dispatcher = dispatcher;
+        this.registry = registry;
+        this.socket = socket;
+
+        asyncEventArgs = new();
+        RegisterHandlers(registry);
+    }
+
+    private void RegisterHandlers(IRegistry registry)
+    {
+
+    }
+
+    private void Connect(Connect connect)
+    {
+        asyncEventArgs.RemoteEndPoint = connect.EndPoint;
+        socket.ConnectAsync(asyncEventArgs);
+    }
+
+    private void UnregisterHandlers(IRegistry registry)
+    {
+
+    }
+
+    public void Dispose() => UnregisterHandlers(registry);
+}
+
+public record struct Connect(EndPoint EndPoint);
+
+public record struct Disconnect;
