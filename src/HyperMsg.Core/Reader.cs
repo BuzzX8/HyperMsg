0724d66857f0b7df36516a05f2b46a91d20f31b6
Maksym Chasnyk <maksym.chasnyk@everymatrix.com>
2023-12-13 22:08:45 +0200

    wip

---
+++ src/HyperMsg.Core/Reader.cs
@@ -0,0 +1,32 @@
+﻿namespace HyperMsg;
+
+public static class Reader
+{
+    public static Func<Result<T>> New<T>(Decoder<T> decoder, Func<Memory<byte>, Result<int>> bufferWriter, Func<Result<Memory<byte>>> bufferProvider)
+    {
+        return () => bufferProvider().Match(
+            Succ: NewMethod(decoder, bufferWriter),
+            Fail: error => new Result<T>(error));
+    }
+
+    private static Func<Memory<byte>, Result<T>> NewMethod<T>(Decoder<T> decoder, Func<Memory<byte>, Result<int>> bufferWriter)
+    {
+        return buffer => bufferWriter(buffer).Match(
+            Succ: NewMethod1(decoder, buffer), 
+            Fail: error => new Result<T>(error));
+    }
+
+    private static Func<int, Result<T>> NewMethod1<T>(Decoder<T> decoder, Memory<byte> buffer)
+    {
+        return bytesWritten => decoder(buffer[..bytesWritten]).Match(
+            Succ: r => new Result<T>(r.message),
+            Fail: error => new Result<T>(error));
+    }
+
+    public static Func<CancellationToken, Task<Result<T>>> NewAsync<T>(Decoder<T> decoder, Func<Memory<byte>, CancellationToken, Task<Result<int>>> bufferWriter, Func<Result<Memory<byte>>> bufferProvider)
+    {
+        return token => bufferProvider().Match(
+            Succ: null,
+            Fail: error => Task.FromResult(new Result<T>(error)));
+    }
+}
---
+++ src/HyperMsg.Core/Writer.cs
@@ -0,0 +1,13 @@
+﻿namespace HyperMsg;
+
+public static class Writer
+{
+    public static Func<T, Result<Unit>> New<T>(Encoder<T> encoder, Func<Memory<byte>, Result<Unit>> bufferWriter, Func<Result<Memory<byte>>> bufferProvider)
+    {
+        return message => bufferProvider().Match(
+            Succ: buffer => encoder(buffer, message).Match(
+                Succ: bytesEncoded => bufferWriter(buffer[..bytesEncoded]),
+                Fail: error => new Result<Unit>(error)),
+            Fail: error => new Result<Unit>(error));
+    }
+}
