90788208d0455f4d01f9ee1f4349879c9e3bf883
Maksym Chasnyk <maksym.chasnyk@everymatrix.com>
2023-12-14 17:18:07 +0200

    wip

--- src/HyperMsg.Core/DecodingPipeline.cs
+++ src/HyperMsg.Core/DecodingPipeline.cs
@@ -10,21 +10,11 @@ public static class DecodingPipeline
     public static Func<Result<T>> New<T>(Decoder<T> decoder, Func<Memory<byte>, Result<int>> bufferWriter, Func<Result<Memory<byte>>> bufferProvider)
     {
         return () => bufferProvider().Match(
-            Succ: New(decoder, bufferWriter),
-            Fail: error => new Result<T>(error));
-    }
-
-    private static Func<Memory<byte>, Result<T>> New<T>(Decoder<T> decoder, Func<Memory<byte>, Result<int>> bufferWriter)
-    {
-        return buffer => bufferWriter(buffer).Match(
-            Succ: New(decoder, buffer),
-            Fail: error => new Result<T>(error));
-    }
-
-    private static Func<int, Result<T>> New<T>(Decoder<T> decoder, Memory<byte> buffer)
-    {
-        return bytesWritten => decoder(buffer[..bytesWritten]).Match(
-            Succ: r => new Result<T>(r.message),
+            Succ: buffer => bufferWriter(buffer).Match(
+                Succ: bytesWritten => decoder(buffer[..bytesWritten]).Match(
+                    Succ: r => new Result<T>(r.message),
+                    Fail: error => new Result<T>(error)),
+                Fail: error => new Result<T>(error)),
             Fail: error => new Result<T>(error));
     }
 
@@ -36,21 +26,11 @@ public static class DecodingPipeline
     public static Func<CancellationToken, ValueTask<Result<T>>> NewAsync<T>(Decoder<T> decoder, Func<Memory<byte>, CancellationToken, ValueTask<Result<int>>> bufferWriter, Func<Result<Memory<byte>>> bufferProvider)
     {
         return token => bufferProvider().Match(
-                Succ: NewAsync(decoder, bufferWriter, token),
-                Fail: error => ValueTask.FromResult(new Result<T>(error)));
-    }
-
-    private static Func<Memory<byte>, ValueTask<Result<T>>> NewAsync<T>(Decoder<T> decoder, Func<Memory<byte>, CancellationToken, ValueTask<Result<int>>> bufferWriter, CancellationToken cancellationToken)
-    {
-        return async (buffer) => await (await bufferWriter(buffer, cancellationToken)).Match(
-                Succ: NewAsync(decoder, buffer),
-                Fail: error => ValueTask.FromResult(new Result<T>(error)));
-    }
-
-    private static Func<int, ValueTask<Result<T>>> NewAsync<T>(Decoder<T> decoder, Memory<byte> buffer)
-    {
-        return (bytesWritten) => decoder(buffer[..bytesWritten]).Match(
-            Succ: r => ValueTask.FromResult(new Result<T>(r.message)),
+            Succ: (Func<Memory<byte>, ValueTask<Result<T>>>)(async (buffer) => await (await bufferWriter(buffer, token)).Match(
+                Succ: bytesWritten => decoder(buffer[..bytesWritten]).Match(
+                    Succ: r => ValueTask.FromResult(new Result<T>(r.message)),
+                    Fail: error => ValueTask.FromResult(new Result<T>(error))),
+                Fail: error => ValueTask.FromResult(new Result<T>(error)))),
             Fail: error => ValueTask.FromResult(new Result<T>(error)));
     }
 }
