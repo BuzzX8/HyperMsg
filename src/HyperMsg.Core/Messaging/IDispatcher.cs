46d2065435d8bd54f0353cb11463713b2cc98f4c
Maxim <maxim.chasnik@outlook.com>
2025-06-18 13:25:43 +0300

    Added MessagingExtensions

---
+++ src/HyperMsg.Core/Messaging/IDispatcher.cs
@@ -0,0 +1,8 @@
+﻿namespace HyperMsg.Messaging;
+
+public interface IDispatcher
+{
+    void Dispatch<T>(T data) where T : notnull;
+
+    Task DispatchAsync<T>(T data, CancellationToken cancellationToken = default) where T : notnull;
+}
---
+++ src/HyperMsg.Core/Messaging/IHandlerRegistry.cs
@@ -0,0 +1,32 @@
+﻿namespace HyperMsg.Messaging;
+
+public interface IHandlerRegistry
+{
+    /// <summary>
+    /// Registers a message handler for a specific message type.
+    /// </summary>
+    /// <typeparam name="T">The type of the message.</typeparam>
+    /// <param name="messageHandler">The handler to register.</param>
+    void Register<T>(Action<T> messageHandler);
+
+    /// <summary>
+    /// Registers a asynchronous message handler for a specific message type.
+    /// </summary>
+    /// <typeparam name="T">The type of the message.</typeparam>
+    /// <param name="asyncMessageHandler">The handler to register.</param>
+    void Register<T>(Func<T, CancellationToken, Task> asyncMessageHandler);
+
+    /// <summary>
+    /// Unregisters a message handler for a specific message type.
+    /// </summary>
+    /// <typeparam name="T">The type of the message.</typeparam>
+    /// <param name="messageHandler">The handler to unregister.</param>
+    void Unregister<T>(Action<T> messageHandler);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T">The type of the message.</typeparam>
+    /// <param name="asyncMessageHandler">The handler to unregister.</param>
+    void Unregister<T>(Func<T, CancellationToken, Task> asyncMessageHandler);
+}
