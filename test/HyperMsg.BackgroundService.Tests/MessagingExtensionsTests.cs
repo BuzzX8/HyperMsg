4bb7a94c55c3b1486b70bdd7ac6414e6c9c75d16
Maxim <maxim.chasnik@outlook.com>
2025-06-19 10:28:40 +0300

    fix

--- test/HyperMsg.BackgroundService.Tests/MessagingExtensionsTests.cs
+++ test/HyperMsg.BackgroundService.Tests/MessagingExtensionsTests.cs
@@ -15,7 +15,6 @@ public class MessagingExtensionsTests
         var builder = Host.CreateApplicationBuilder();
         
         // Act
-        builder.Services.AddMessagingWorker();
         var host = builder.Build();
 
         // Assert
