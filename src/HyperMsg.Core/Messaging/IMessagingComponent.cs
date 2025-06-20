9d4b5582532915b848fbce61a5d0a383fb083157
BuzzX8 <maxim.chasnik@gmail.com>
2025-06-20 21:27:26 +0300

    Added MessagingComponent

---
+++ src/HyperMsg.Core/Messaging/IMessagingComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace HyperMsg.Messaging;
+
+/// <summary>
+/// 
+/// </summary>
+public interface IMessagingComponent
+{
+    void Attach(IMessagingContext messagingContext);
+
+    void Detach(IMessagingContext messagingContext);
+}
