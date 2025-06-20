3d8c7c7f32854c5fea65b47e5fd5465918164b65
BuzzX8 <maxim.chasnik@gmail.com>
2025-06-20 21:54:49 +0300

    Added some tests

---
+++ test/HyperMsg.Core.Tests/Messaging/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using FakeItEasy;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace HyperMsg.Messaging;
+
+public class ServiceCollectionExtensionsTests
+{
+    private readonly ServiceCollection services;
+
+    public ServiceCollectionExtensionsTests()
+    {
+        services = new ServiceCollection();
+    }
+
+    [Fact]
+    public void AddMessageBroker_ShouldRegisterServices()
+    {
+        // Act
+        services.AddMessagingContext();
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        Assert.NotNull(serviceProvider.GetService<IDispatcher>());
+        Assert.NotNull(serviceProvider.GetService<IHandlerRegistry>());
+        Assert.NotNull(serviceProvider.GetService<IMessagingContext>());
+    }
+
+    [Fact]
+    public void AddMessagingComponent_()
+    {
+        var component = A.Fake<IMessagingComponent>();
+        services.AddMessagingComponent(component);
+
+        var serviceProvider = services.BuildServiceProvider();
+        var registeredComponent = serviceProvider.GetService<IMessagingComponent>();
+
+        Assert.NotNull(registeredComponent);
+        Assert.Same(component, registeredComponent);
+    }
+}
