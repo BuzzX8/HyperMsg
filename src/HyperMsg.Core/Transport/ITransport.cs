211231407e29b6e175f6e7b79b0a6c355005eda4
Max Gunther <maxim.chasnik@gmail.com>
2025-06-22 19:52:34 +0300

    Added transport namespace (#219)

---
+++ src/HyperMsg.Core/Transport/ITransport.cs
@@ -0,0 +1,21 @@
+﻿namespace HyperMsg.Transport;
+
+public interface ITransport : IAsyncDisposable
+{
+    /// <summary>
+    /// begins receiving and pushing data to InputBuffer
+    /// <param name="cancellationToken">Cancellation token to cancel the operation.</param>
+    /// </summary>
+    Task StartAsync(CancellationToken cancellationToken);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task SendAsync(ReadOnlyMemory<byte> data, CancellationToken cancellationToken);
+        
+    event Action<Exception> OnError;
+    event Action OnDisconnected;
+}
---
+++ src/HyperMsg.Core/Transport/WebSocketTransport.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Net.WebSockets;
+
+namespace HyperMsg.Transport;
+
+public class WebSocketTransport(WebSocket webSocket) : ITransport
+{
+    private readonly WebSocket _webSocket = webSocket;
+
+    public event Action<Exception> OnError;
+    public event Action OnDisconnected;
+
+    public ValueTask DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SendAsync(ReadOnlyMemory<byte> data, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
