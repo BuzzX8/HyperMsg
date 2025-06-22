28c7cee49a17480c1de129904eaa28fb6fc048d5
Max Gunther <maxim.chasnik@gmail.com>
2025-06-22 19:23:30 +0300

    Buffering Context (#218)

---
+++ src/HyperMsg.Core/Buffers/BufferFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.Buffers;
+
+namespace HyperMsg.Buffers;
+
+public static class BufferFactory
+{
+    public static IBufferingContext NewContext(IMemoryOwner<byte> memoryOwner)
+    {
+        // Create a new instance of BufferingContext
+        return new BufferingContext(memoryOwner.Memory);
+    }
+
+    public static IBufferingContext NewContext(Memory<byte> memory)
+    {
+        // Create a new instance of BufferingContext with the provided Memory<byte>
+        return new BufferingContext(memory);
+    }
+
+    public static IBuffer NewBuffer(IMemoryOwner<byte> memoryOwner)
+    {
+        // Create a new Buffer instance using the provided IMemoryOwner<byte>
+        return new Buffer(memoryOwner.Memory);
+    }
+
+    public static IBuffer NewBuffer(Memory<byte> memory)
+    {
+        // Create a new Buffer instance using the provided Memory<byte>
+        return new Buffer(memory);
+    }
+}
---
+++ src/HyperMsg.Core/Buffers/IBuffer.cs
@@ -0,0 +1,21 @@
+namespace HyperMsg.Buffers;
+
+/// <summary>
+/// Represents a buffer that provides both reading and writing capabilities, with reactive behavior.
+/// </summary>
+public interface IBuffer : IBufferReader, IBufferWriter
+{
+    IBufferReader Reader { get; }
+    IBufferWriter Writer { get; }
+    void Clear();
+
+    /// <summary>
+    /// Occurs when data is written to the buffer.
+    /// </summary>
+    event Action<int> DataWritten;
+
+    /// <summary>
+    /// Occurs when data is read from the buffer.
+    /// </summary>
+    event Action<int> DataRead;
+}
