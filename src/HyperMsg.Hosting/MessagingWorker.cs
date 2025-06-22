c211d603f48cb4bdfd7bf24fc909edf37088e017
BuzzX8 <maxim.chasnik@gmail.com>
2025-06-22 20:52:21 +0300

    updated message worker

--- src/HyperMsg.Hosting/MessagingWorker.cs
+++ src/HyperMsg.Hosting/MessagingWorker.cs
@@ -2,14 +2,15 @@ using HyperMsg.Messaging;
 
 namespace HyperMsg.Hosting;
 
-public class MessagingWorker(IMessagingContext messagingContext, ILogger<MessagingWorker> logger) : BackgroundService
+public class MessagingWorker(IMessagingContext messagingContext, IEnumerable<IMessagingComponent> components, ILogger<MessagingWorker> logger) : BackgroundService
 {
     private readonly IMessagingContext messagingContext = messagingContext;
+    private readonly IMessagingComponent[] components = [.. components];
     private readonly ILogger<MessagingWorker> _logger = logger;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        await RegisterHandlersAsync(messagingContext.Dispatcher, messagingContext.HandlerRegistry, stoppingToken);
+        await RegisterHandlersAsync(messagingContext, stoppingToken);
 
         while (!stoppingToken.IsCancellationRequested)
         {
@@ -17,15 +18,33 @@ public class MessagingWorker(IMessagingContext messagingContext, ILogger<Messagi
         }
     }
 
-    protected virtual Task RegisterHandlersAsync(IDispatcher dispatcher, IHandlerRegistry registry, CancellationToken cancellationToken)
+    protected virtual Task RegisterHandlersAsync(IMessagingContext messagingContext, CancellationToken cancellationToken)
     {
-        // Override this method in derived classes to register message handlers
+        // Register components with the messaging context
+        for (int i = 0; i < components.Length; i++)
+        {
+            var component = components[i];
+            component.Attach(messagingContext);
+        }
+
         return Task.CompletedTask;
     }
 
-    public virtual Task OnHeartBeatAsync(CancellationToken cancellationToken)
+    protected virtual Task UnregisterHandlersAsync(IMessagingContext messagingContext, CancellationToken cancellationToken)
+    {
+        // Unregister components from the messaging context
+        for (int i = 0; i < components.Length; i++)
+        {
+            var component = components[i];
+            component.Detach(messagingContext);
+        }
+        return Task.CompletedTask;
+    }
+
+    protected virtual Task OnHeartBeatAsync(CancellationToken cancellationToken)
     {
         // Override this method in derived classes to handle heartbeat events
+        _logger.LogInformation("Heartbeat event triggered at {Time}", DateTimeOffset.Now);
         return Task.CompletedTask;
     }
 }
