097dccdb8109f473fd21a3e5f910d6ef700a3aa5
BuzzX8 <maxim.chasnik@gmail.com>
2025-07-06 18:32:06 +0300

    added BufferingIntegrationTests

---
+++ test/HyperMsg.Integration.Tests/BufferingIntegrationTests.cs
@@ -0,0 +1,34 @@
+﻿using HyperMsg.Buffers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HyperMsg.Integration.Tests;
+
+public class BufferingIntegrationTests : IDisposable
+{
+    private readonly IHost _host;
+
+    public BufferingIntegrationTests()
+    {
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                services.AddBufferingContext();
+            })
+            .Build();
+        _host.StartAsync();
+    }
+
+    [Fact]
+    public void BufferingContext_ShouldBeAvailable()
+    {
+        var bufferingContext = _host.Services.GetService<IBufferingContext>();
+        Assert.NotNull(bufferingContext);
+    }
+
+    void IDisposable.Dispose()
+    {
+        _host.StopAsync(TimeSpan.FromSeconds(5)).GetAwaiter().GetResult();
+        _host.Dispose();
+    }
+}
