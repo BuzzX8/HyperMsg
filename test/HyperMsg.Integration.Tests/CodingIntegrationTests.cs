4b6874945f65cfb1b47b66c812a0ccb18e90cee7
BuzzX8 <maxim.chasnik@gmail.com>
2025-07-08 18:07:46 +0300

    wip

--- test/HyperMsg.Integration.Tests/CodingIntegrationTests.cs
+++ test/HyperMsg.Integration.Tests/CodingIntegrationTests.cs
@@ -8,6 +8,23 @@ public class CodingIntegrationTests : IntegrationTestsBase
     {
     }
 
+    [Fact]
+    public void CodingContext_ProvidesEncoderAndDecoder()
+    {
+        var codingContext = GetRequiredService<ICodingContext<Guid>>();
+        var buffer = new byte[100];
+        var message = Guid.NewGuid();
+
+        // Encode the message
+        var bytesEncoded = codingContext.Encoder.Invoke(buffer, message);
+        Assert.Equal(16u, bytesEncoded);
+
+        // Decode the message
+        var decodingResult = codingContext.Decoder.Invoke(buffer);
+        Assert.Equal(message, decodingResult.Message);
+        Assert.Equal(16UL, decodingResult.BytesDecoded);
+    }
+
     private static ulong EncodeGuid(Memory<byte> buffer, Guid message)
     {
         var bytes = message.ToByteArray();
@@ -17,6 +34,6 @@ public class CodingIntegrationTests : IntegrationTestsBase
 
     private static DecodingResult<Guid> DecodeGuid(ReadOnlyMemory<byte> buffer)
     {
-        return new(new(buffer.Span), 16);
+        return new(new(buffer.Span[..16]), 16);
     }
 }
