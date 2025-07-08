96fdf4cd80597a64ad03880578afa9bba53fa0f2
BuzzX8 <maxim.chasnik@gmail.com>
2025-07-08 09:29:28 +0300

    wip

--- src/HyperMsg.Core/Coding/DecodingResult.cs
+++ src/HyperMsg.Core/Coding/DecodingResult.cs
@@ -1,3 +1,9 @@
 ﻿namespace HyperMsg.Coding;
 
+/// <summary>
+/// Represents the result of a decoding operation, including the decoded message and the number of bytes processed.
+/// </summary>
+/// <typeparam name="T">The type of the decoded message.</typeparam>
+/// <param name="Message">The decoded message instance.</param>
+/// <param name="BytesDecoded">The number of bytes that were successfully decoded.</param>
 public readonly record struct DecodingResult<T>(T Message, ulong BytesDecoded);
