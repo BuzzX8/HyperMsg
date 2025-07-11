30944a3d217895bf800d710808eb553ea35dd328
Maxim <maxim.chasnik@outlook.com>
2025-07-11 11:05:23 +0300

    wip

---
+++ src/HyperMsg.Core/Messaging/DispatcherExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace HyperMsg.Messaging;
+
+public static class DispatcherExtensions
+{
+    public static TResponse DispatchRequest<TRequest, TResponse>(this IDispatcher dispatcher, TRequest request)
+    {
+        var message = new RequestResponse<TRequest, TResponse>(request, default);
+        dispatcher.Dispatch(message);
+        return message.Response;
+    }
+
+    public static async Task<TResponse> DispatchRequestAsync<TRequest, TResponse>(this IDispatcher dispatcher, TRequest request, CancellationToken cancellationToken = default)
+    {
+        var message = new RequestResponse<TRequest, TResponse>(request, default);
+        await dispatcher.DispatchAsync(message, cancellationToken);
+        return message.Response;
+    }
+}
