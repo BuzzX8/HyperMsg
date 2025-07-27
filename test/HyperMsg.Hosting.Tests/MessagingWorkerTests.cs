94ea3294b8db64dcb44ce67e07134d1a47877e96
BuzzX8 <maxim.chasnik@gmail.com>
2025-07-27 19:49:30 +0300

    wip

--- test/HyperMsg.Hosting.Tests/MessagingWorkerTests.cs
+++ test/HyperMsg.Hosting.Tests/MessagingWorkerTests.cs
@@ -1,5 +1,6 @@
 ﻿using FakeItEasy;
 using HyperMsg.Messaging;
+using Microsoft.Extensions.Logging;
 
 namespace HyperMsg.Hosting.Tests;
 
@@ -8,12 +9,13 @@ public class MessagingWorkerTests
     private readonly MessageBroker _broker;
     private readonly List<IMessagingComponent> _components;
     private readonly MessagingWorker _worker;
+    private readonly ILogger<MessagingWorker> _logger = A.Fake<ILogger<MessagingWorker>>();
 
     public MessagingWorkerTests()
     {
         _broker = new();
         _components = [];
-        _worker = new MessagingWorker(_broker, _components);
+        _worker = new MessagingWorker(_broker, _components, _logger);
     }
 
     [Fact]
@@ -23,16 +25,26 @@ public class MessagingWorkerTests
         var componentFake = A.Fake<IMessagingComponent>();
         _components.Add(componentFake);
 
-        var worker = new MessagingWorker(_broker, _components);
-
-        using var cts = new CancellationTokenSource();
-        cts.CancelAfter(100); // Cancel quickly to exit ExecuteAsync
-
         // Act
-        await worker.StartAsync(cts.Token);
+        await _worker.StartAsync(default);
 
         // Assert
         A.CallTo(() => componentFake.Attach(_broker)).MustHaveHappened();
         A.CallTo(() => componentFake.Detach(_broker)).MustNotHaveHappened();
+    }
+
+    [Fact]
+    public async Task StopAsync_UnregistersComponents()
+    {
+        // Arrange
+        var componentFake = A.Fake<IMessagingComponent>();
+        _components.Add(componentFake);
+        using var cts = new CancellationTokenSource();
+        cts.CancelAfter(100); // Cancel quickly to exit ExecuteAsync
+        // Act
+        await _worker.StartAsync(cts.Token);
+        await _worker.StopAsync(cts.Token);
+        // Assert
+        A.CallTo(() => componentFake.Detach(_broker)).MustHaveHappened();
     }
 }
