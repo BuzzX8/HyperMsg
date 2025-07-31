09ef5d684c26ee3b54da7dc83e6d8b0604b0bd2e
Max Gunther <maxim.chasnik@gmail.com>
2025-07-31 13:53:32 +0300

    Service collection update (#233)

--- src/HyperMsg.Core/Messaging/ServiceCollectionExtensions.cs
+++ src/HyperMsg.Core/Messaging/ServiceCollectionExtensions.cs
@@ -2,20 +2,91 @@
 
 namespace HyperMsg.Messaging;
 
+/// <summary>
+/// Provides extension methods for registering messaging services and components in the dependency injection container.
+/// </summary>
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddMessagingContext(this IServiceCollection services)
+    /// <summary>
+    /// Registers a synchronous message handler for messages of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of message the handler processes.</typeparam>
+    /// <param name="services">The service collection to add the handler to.</param>
+    /// <param name="messageHandler">The message handler delegate.</param>
+    /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddMessageHandler<T>(this IServiceCollection services, MessageHandler<T> messageHandler)
     {
-        var messageBroker = new MessageBroker();
-        
-        return services.AddSingleton<IDispatcher>(messageBroker)
-            .AddSingleton<IHandlerRegistry>(messageBroker)
-            .AddSingleton<IMessagingContext>(messageBroker);
+        MessagingContextConfigurator configurator = (context) =>
+        {
+            context.HandlerRegistry.RegisterHandler(messageHandler);
+        };
+        return services.AddSingleton(configurator);
     }
 
-    public static IServiceCollection AddMessagingComponent<T>(this IServiceCollection services) where T : class, IMessagingComponent 
+    /// <summary>
+    /// Registers an asynchronous message handler for messages of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of message the handler processes.</typeparam>
+    /// <param name="services">The service collection to add the handler to.</param>        
+    /// <param name="messageHandler">The asynchronous message handler delegate.</param>
+    /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddAsyncMessageHandler<T>(this IServiceCollection services, AsyncMessageHandler<T> messageHandler)
+    {
+        MessagingContextConfigurator configurator = (context) =>
+        {
+            context.HandlerRegistry.RegisterHandler(messageHandler);
+        };
+        return services.AddSingleton(configurator);
+    }
+
+    /// <summary>
+    /// Registers the messaging context and its related services (<see cref="IDispatcher"/>, <see cref="IHandlerRegistry"/>, <see cref="IMessagingContext"/>) as singletons in the service collection.
+    /// Configurators can be used to register handlers or perform additional setup on the <see cref="IMessagingContext"/>.
+    /// </summary>
+    /// <param name="services">The service collection to add the messaging context to.</param>
+    /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddMessagingContext(this IServiceCollection services)
+    {
+        return services.AddSingleton<IDispatcher, MessageBroker>(provider =>
+        {
+            var messageBroker = new MessageBroker();
+
+            var handlers = provider.GetServices<MessagingContextConfigurator>();
+
+            foreach (var handler in handlers)
+            {
+                handler.Invoke(messageBroker);
+            }
+
+            return messageBroker;
+        }
+        )
+            .AddSingleton<IHandlerRegistry, MessageBroker>()
+            .AddSingleton<IMessagingContext, MessageBroker>();
+    }
+
+    /// <summary>
+    /// Registers a messaging component of type <typeparamref name="T"/> as a singleton implementation of <see cref="IMessagingComponent"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the messaging component to register.</typeparam>
+    /// <param name="services">The service collection to add the messaging component to.</param>
+    /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddMessagingComponent<T>(this IServiceCollection services) where T : class, IMessagingComponent
         => services.AddSingleton<IMessagingComponent, T>();
 
-    public static IServiceCollection AddMessagingComponent<T>(this IServiceCollection services, T component) where T : class, IMessagingComponent 
+    /// <summary>
+    /// Registers the specified messaging component instance as a singleton implementation of <see cref="IMessagingComponent"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the messaging component.</typeparam>
+    /// <param name="services">The service collection to add the messaging component to.</param>
+    /// <param name="component">The messaging component instance to register.</param>
+    /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddMessagingComponent<T>(this IServiceCollection services, T component) where T : class, IMessagingComponent
         => services.AddSingleton<IMessagingComponent>(component);
 }
+
+/// <summary>
+/// Delegate for configuring the <see cref="IMessagingContext"/> during service registration.
+/// </summary>
+/// <param name="messagingContext">The messaging context to configure.</param>
+public delegate void MessagingContextConfigurator(IMessagingContext messagingContext);
--- test/HyperMsg.Core.Tests/Messaging/ServiceCollectionExtensionsTests.cs
+++ test/HyperMsg.Core.Tests/Messaging/ServiceCollectionExtensionsTests.cs
@@ -27,7 +27,7 @@ public class ServiceCollectionExtensionsTests
     }
 
     [Fact]
-    public void AddMessagingComponent_()
+    public void AddMessagingComponent_Registers_Component()
     {
         var component = A.Fake<IMessagingComponent>();
         services.AddMessagingComponent(component);
@@ -37,5 +37,41 @@ public class ServiceCollectionExtensionsTests
 
         Assert.NotNull(registeredComponent);
         Assert.Same(component, registeredComponent);
+    }
+
+    [Fact]
+    public void AddMessageHandler_Registers_Configurator()
+    {
+        // Arrange
+        var handler = A.Fake<MessageHandler<string>>();
+        services.AddMessageHandler(handler);
+        services.AddMessagingContext();
+
+        var serviceProvider = services.BuildServiceProvider();
+        var dispatcher = serviceProvider.GetService<IDispatcher>();
+
+        // Act
+        dispatcher.Dispatch("test message");
+
+        // Assert
+        A.CallTo(handler).MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public void AddAsyncMessageHandler_Registers_Configurator()
+        {
+        // Arrange
+        var asyncHandler = A.Fake<AsyncMessageHandler<string>>();
+        services.AddAsyncMessageHandler(asyncHandler);
+        services.AddMessagingContext();
+
+        var serviceProvider = services.BuildServiceProvider();
+        var dispatcher = serviceProvider.GetService<IDispatcher>();
+
+        // Act
+        dispatcher.DispatchAsync("test message").GetAwaiter().GetResult();
+
+        // Assert
+        A.CallTo(() => asyncHandler.Invoke("test message", A<CancellationToken>._)).MustHaveHappenedOnceExactly();
     }
 }
