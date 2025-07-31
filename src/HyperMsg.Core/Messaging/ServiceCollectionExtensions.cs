8e1296e609b70e8df0f449c1006307c76dc57025
BuzzX8 <maxim.chasnik@gmail.com>
2025-07-31 12:29:12 +0300

    wip

--- src/HyperMsg.Core/Messaging/ServiceCollectionExtensions.cs
+++ src/HyperMsg.Core/Messaging/ServiceCollectionExtensions.cs
@@ -8,20 +8,61 @@ namespace HyperMsg.Messaging;
 public static class ServiceCollectionExtensions
 {
     /// <summary>
+    /// Registers a synchronous message handler for messages of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of message the handler processes.</typeparam>
+    /// <param name="services">The service collection to add the handler to.</param>
+    /// <param name="messageHandler">The message handler delegate.</param>
+    /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddMessageHandler<T>(this IServiceCollection services, MessageHandler<T> messageHandler)
+    {
+        MessagingContextConfigurator configurator = (context) =>
+        {
+            context.HandlerRegistry.RegisterHandler(messageHandler);
+        };
+        return services.AddSingleton(configurator);
+    }
+
+    /// <summary>
+    /// Registers an asynchronous message handler for messages of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of message the handler processes.</typeparam>
+    /// <param name="services">The service collection to add the handler to.</param>        
+    /// <param name="messageHandler">The asynchronous message handler delegate.</param>
+    /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddAsyncMessageHandler<T>(this IServiceCollection services, AsyncMessageHandler<T> messageHandler)
+    {
+        MessagingContextConfigurator configurator = (context) =>
+        {
+            context.HandlerRegistry.RegisterHandler(messageHandler);
+        };
+        return services.AddSingleton(configurator);
+    }
+
+    /// <summary>
     /// Registers the messaging context and its related services (<see cref="IDispatcher"/>, <see cref="IHandlerRegistry"/>, <see cref="IMessagingContext"/>) as singletons in the service collection.
+    /// Configurators can be used to register handlers or perform additional setup on the <see cref="IMessagingContext"/>.
     /// </summary>
     /// <param name="services">The service collection to add the messaging context to.</param>
-    /// <param name="configurator">An optional delegate to configure the <see cref="IMessagingContext"/> instance.</param>
     /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
-    public static IServiceCollection AddMessagingContext(this IServiceCollection services, Action<IMessagingContext> configurator = null)
-    {        
-        var messageBroker = new MessageBroker();
+    public static IServiceCollection AddMessagingContext(this IServiceCollection services)
+    {
+        return services.AddSingleton<IDispatcher, MessageBroker>(provider =>
+        {
+            var messageBroker = new MessageBroker();
 
-        configurator?.Invoke(messageBroker);
+            var handlers = provider.GetServices<MessagingContextConfigurator>();
 
-        return services.AddSingleton<IDispatcher>(messageBroker)
-            .AddSingleton<IHandlerRegistry>(messageBroker)
-            .AddSingleton<IMessagingContext>(messageBroker);
+            foreach (var handler in handlers)
+            {
+                handler.Invoke(messageBroker);
+            }
+
+            return messageBroker;
+        }
+        )
+            .AddSingleton<IHandlerRegistry, MessageBroker>()
+            .AddSingleton<IMessagingContext, MessageBroker>();
     }
 
     /// <summary>
@@ -30,7 +71,7 @@ public static class ServiceCollectionExtensions
     /// <typeparam name="T">The type of the messaging component to register.</typeparam>
     /// <param name="services">The service collection to add the messaging component to.</param>
     /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
-    public static IServiceCollection AddMessagingComponent<T>(this IServiceCollection services) where T : class, IMessagingComponent 
+    public static IServiceCollection AddMessagingComponent<T>(this IServiceCollection services) where T : class, IMessagingComponent
         => services.AddSingleton<IMessagingComponent, T>();
 
     /// <summary>
@@ -40,6 +81,12 @@ public static class ServiceCollectionExtensions
     /// <param name="services">The service collection to add the messaging component to.</param>
     /// <param name="component">The messaging component instance to register.</param>
     /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
-    public static IServiceCollection AddMessagingComponent<T>(this IServiceCollection services, T component) where T : class, IMessagingComponent 
+    public static IServiceCollection AddMessagingComponent<T>(this IServiceCollection services, T component) where T : class, IMessagingComponent
         => services.AddSingleton<IMessagingComponent>(component);
 }
+
+/// <summary>
+/// Delegate for configuring the <see cref="IMessagingContext"/> during service registration.
+/// </summary>
+/// <param name="messagingContext">The messaging context to configure.</param>
+public delegate void MessagingContextConfigurator(IMessagingContext messagingContext);
