8c7ad07d483e6b4f2d79cdda85e6f968fdb32761
Max Gunther <maxim.chasnik@gmail.com>
2025-11-05 09:17:08 +0200

    wip

--- src/HyperMsg.Messaging/IMessagingComponent.cs
+++ src/HyperMsg.Messaging/IMessagingComponent.cs
@@ -1,11 +1,26 @@
 ﻿namespace HyperMsg.Messaging;
 
 /// <summary>
-/// 
+/// Represents a component that can participate in the messaging system by
+/// attaching to an <see cref="IMessagingContext"/> to register handlers and
+/// detaching to release resources or unregister handlers.
 /// </summary>
 public interface IMessagingComponent
 {
+    /// <summary>
+    /// Attach the component to the provided messaging context. Implementations
+    /// should register any required message handlers or other callbacks against
+    /// <paramref name="messagingContext"/> during attachment.
+    /// </summary>
+    /// <param name="messagingContext">The messaging context that exposes the dispatcher
+    /// and handler registry used by the component.</param>
     void Attach(IMessagingContext messagingContext);
 
+    /// <summary>
+    /// Detach the component from the provided messaging context. Implementations
+    /// should unregister handlers and perform any necessary cleanup so the component
+    /// no longer receives messages from <paramref name="messagingContext"/>.
+    /// </summary>
+    /// <param name="messagingContext">The messaging context the component is detaching from.</param>
     void Detach(IMessagingContext messagingContext);
 }
---
+++ test/HyperMsg.Core.Tests/Messaging/MessagingComponentBaseTests.cs
@@ -0,0 +1,59 @@
+﻿using FakeItEasy;
+using Xunit;
+
+namespace HyperMsg.Messaging;
+
+public class MessagingComponentBaseTests
+{
+    [Fact]
+    public void Attach_Calls_RegisterHandlers_With_Context()
+    {
+        var context = A.Fake<IMessagingContext>();
+        var component = new TestComponent([]);
+
+        component.Attach(context);
+
+        Assert.Same(context, component.CapturedContext);
+    }
+
+    [Fact]
+    public void Dispose_Disposes_Returned_Disposables()
+    {
+        var disposable1 = A.Fake<IDisposable>();
+        var disposable2 = A.Fake<IDisposable>();
+        var component = new TestComponent([disposable1, disposable2]);
+
+        component.Attach(A.Fake<IMessagingContext>());
+
+        component.Dispose();
+
+        A.CallTo(() => disposable1.Dispose()).MustHaveHappenedOnceExactly();
+        A.CallTo(() => disposable2.Dispose()).MustHaveHappenedOnceExactly();
+    }
+
+    [Fact]
+    public void Detach_Invokes_Dispose_On_Disposables()
+    {
+        var disposable = A.Fake<IDisposable>();
+        var component = new TestComponent([disposable]);
+
+        component.Attach(A.Fake<IMessagingContext>());
+
+        component.Detach(A.Fake<IMessagingContext>());
+
+        A.CallTo(() => disposable.Dispose()).MustHaveHappenedOnceExactly();
+    }
+
+    private sealed class TestComponent(IEnumerable<IDisposable> handlers) : MessagingComponentBase
+    {
+        private readonly IEnumerable<IDisposable> handlers = handlers;
+
+        public IMessagingContext? CapturedContext { get; private set; }
+
+        protected override IEnumerable<IDisposable> RegisterHandlers(IMessagingContext messagingContext)
+        {
+            CapturedContext = messagingContext;
+            return handlers;
+        }
+    }
+}
