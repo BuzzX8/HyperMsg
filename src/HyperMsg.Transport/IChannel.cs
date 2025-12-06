4f223deaf9a49e46f07b7c9a01e24fcbea8ee857
BuzzX8 <maxim.chasnik@gmail.com>
2025-12-06 18:51:07 +0200

    wip

---
+++ src/HyperMsg.Transport/IChannel.cs
@@ -0,0 +1,8 @@
+﻿namespace HyperMsg.Transport;
+
+public interface IChannel
+{    
+    Task<int> SendAsync(ReadOnlyMemory<byte> data, CancellationToken cancellationToken = default);
+
+    Task<int> ReceiveAsync(Memory<byte> data, CancellationToken cancellationToken = default);
+}
--- src/HyperMsg.Transport/ITransportContext.cs
+++ src/HyperMsg.Transport/ITransportContext.cs
@@ -11,26 +11,7 @@ public interface ITransportContext
     /// <summary>
     /// Gets the connection associated with the transport context.
     /// </summary>
-    IConnection Connection { get; }
-    
-    /// <summary>
-    /// Gets the collection of handlers that are invoked when data is received.
-    /// </summary>
-    ICollection<ReceiveDataHandler> ReceiveDataHandlers { get; }
+    IConnection? Connection { get; }
 
-    /// <summary>
-    /// Asynchronously sends data over the transport connection.
-    /// </summary>
-    /// <param name="data">The data to send as a read-only memory buffer.</param>
-    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
-    /// <returns>A task that represents the asynchronous send operation.</returns>
-    Task SendAsync(ReadOnlyMemory<byte> data, CancellationToken cancellationToken = default);
+    IChannel Channel { get; }
 }
-
-/// <summary>
-/// Represents a handler for processing received data asynchronously.
-/// </summary>
-/// <param name="data">The received data as a read-only memory buffer.</param>
-/// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
-/// <returns>A ValueTask that represents the asynchronous operation.</returns>
-public delegate ValueTask ReceiveDataHandler(ReadOnlyMemory<byte> data, CancellationToken cancellationToken);
