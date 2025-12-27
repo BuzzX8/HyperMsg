4b9d50259dea261c4f043730ad7a4e306bdb8f19
BuzzX8 <maxim.chasnik@gmail.com>
2025-12-27 18:44:57 +0200

    wip

--- src/HyperMsg.Transport.Sockets/SocketTransport.cs
+++ src/HyperMsg.Transport.Sockets/SocketTransport.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Sockets;
+﻿using HyperMsg.Buffers;
+using System.Net.Sockets;
 
 namespace HyperMsg.Transport.Sockets;
 
@@ -8,10 +9,34 @@ namespace HyperMsg.Transport.Sockets;
 /// <remarks>
 /// This class manages the lifecycle and data transmission of a <see cref="Socket"/> connection, including state management and event notification.
 /// </remarks>
-public class SocketTransport(ISocket socket) : ITransportContext, IDisposable
+public class SocketTransport : ITransportContext, IDisposable
 {
-    private readonly SocketConnection _connection = new(socket);
-    private readonly SocketChannel _channel = new(socket);
+    private readonly SocketConnection _connection;
+    private readonly SocketChannel _channel;
+    private readonly IBufferingContext? bufferingContext;
+
+    public SocketTransport(ISocket socket, IBufferingContext? bufferingContext=null)
+    {
+        _connection = new(socket);
+        _channel = new(socket);
+
+        this.bufferingContext = bufferingContext;
+        if (bufferingContext is not null)
+        {
+            bufferingContext.InputBufferUpstreamUpdateRequested += BufferingContext_InputBufferUpstreamUpdateRequested;
+            bufferingContext.OutputBufferDownstreamUpdateRequested += BufferingContext_OutputBufferDownstreamUpdateRequested;
+        }
+    }
+
+    private async ValueTask BufferingContext_OutputBufferDownstreamUpdateRequested(IBuffer buffer, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    private ValueTask BufferingContext_InputBufferUpstreamUpdateRequested(IBuffer buffer, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 
     #region ITransportContext Members
 
