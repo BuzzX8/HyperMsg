c3fa8ee96d02184ec4bf89502b260be6763c9acb
Max Gunther <maxim.chasnik@gmail.com>
2025-12-30 20:14:19 +0200

    socket "integration" with buffer (#246)
    * wip
    
    * wip
    
    * wip
    
    * wip

--- src/HyperMsg.Transport.Sockets/SocketTransport.cs
+++ src/HyperMsg.Transport.Sockets/SocketTransport.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Sockets;
+﻿using HyperMsg.Buffers;
+using System.Net.Sockets;
 
 namespace HyperMsg.Transport.Sockets;
 
@@ -8,10 +9,40 @@ namespace HyperMsg.Transport.Sockets;
 /// <remarks>
 /// This class manages the lifecycle and data transmission of a <see cref="Socket"/> connection, including state management and event notification.
 /// </remarks>
-public class SocketTransport(ISocket socket) : ITransportContext, IDisposable
+public class SocketTransport : ITransportContext, IDisposable
 {
-    private readonly SocketConnection _connection = new(socket);
-    private readonly SocketChannel _channel = new(socket);
+    private readonly ISocket _socket;
+    private readonly SocketConnection _connection;
+    private readonly SocketChannel _channel;
+    private readonly IBufferingContext? bufferingContext;
+
+    public SocketTransport(ISocket socket, IBufferingContext? bufferingContext=null)
+    {
+        _socket = socket;
+        _connection = new(socket);
+        _channel = new(socket);
+
+        this.bufferingContext = bufferingContext;
+        if (bufferingContext is not null)
+        {
+            bufferingContext.InputBufferUpstreamUpdateRequested += BufferingContext_InputBufferUpstreamUpdateRequested;
+            bufferingContext.OutputBufferDownstreamUpdateRequested += BufferingContext_OutputBufferDownstreamUpdateRequested;
+        }
+    }
+
+    private async ValueTask BufferingContext_OutputBufferDownstreamUpdateRequested(IBuffer buffer, CancellationToken cancellationToken)
+    {
+        var memory = buffer.Reader.GetMemory();
+        var bytesSent = await _socket.SendAsync(memory, cancellationToken);
+        buffer.Reader.Advance(bytesSent);
+    }
+
+    private async ValueTask BufferingContext_InputBufferUpstreamUpdateRequested(IBuffer buffer, CancellationToken cancellationToken)
+    {
+        var memory = buffer.Writer.GetMemory();
+        var bytesReceived = await _socket.ReceiveAsync(memory, cancellationToken);
+        buffer.Writer.Advance(bytesReceived);
+    }
 
     #region ITransportContext Members
 
@@ -22,8 +53,10 @@ public class SocketTransport(ISocket socket) : ITransportContext, IDisposable
 
     #endregion
 
-    public void Dispose()
+    public void Dispose() 
     {
         _connection.Dispose();
+        bufferingContext?.InputBufferUpstreamUpdateRequested -= BufferingContext_InputBufferUpstreamUpdateRequested;
+        bufferingContext?.OutputBufferDownstreamUpdateRequested -= BufferingContext_OutputBufferDownstreamUpdateRequested;
     }
 }
